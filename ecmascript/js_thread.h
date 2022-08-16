6f2c31a15270e3bec6bf497c60bef6078b96db69
zhangyukun8 <zhangyukun8@huawei.com>
2022-08-16 17:04:08 +0800

    fixed 01c616b from https://gitee.com/zhangyukun8/ark_js_runtime/pulls/2037 Fix cpuprofile crash of applications
    1.asm stack size is determined by both thread stack size and max value
    of stack size
    
    Issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I5LYD1?from=project-issue
    
    Signed-off-by: zhangyukun8 <zhangyukun8@huawei.com>
    Change-Id: Ib19bcc9f52b80f6fd22c45429998e06f9d7b89c0

--- ecmascript/js_thread.h
+++ ecmascript/js_thread.h
@@ -703,6 +703,8 @@ private:
 
     void DumpStack() DUMP_API_ATTR;
 
+    static size_t GetAsmStackSize();
+
     static const uint32_t NODE_BLOCK_SIZE_LOG2 = 10;
     static const uint32_t NODE_BLOCK_SIZE = 1U << NODE_BLOCK_SIZE_LOG2;
     static constexpr int32_t MIN_HANDLE_STORAGE_SIZE = 2;
