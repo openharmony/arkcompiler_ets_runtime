5745df0c3b05ea731d94e741bdd2feb4b933f451
hongbinj <jinhongbin2@huawei.com>
2022-09-07 15:30:27 +0800

    Add type information to "this" parameter
    1. Associate type information with function parameters by using class/function type.
    2. Fix incorrect GC for objects of "TSFunctionType".
    
    Issue:
    https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/I5PV3L
    
    Test:
    typeinfertest, aot262test
    
    Signed-off-by: hongbinj <jinhongbin2@huawei.com>
    Change-Id: Ib963b0c8e28e629dcb6d534ac10c57b418fa6595

--- ecmascript/ts_types/ts_manager.h
+++ ecmascript/ts_types/ts_manager.h
@@ -164,6 +164,8 @@ public:
 
     GlobalTSTypeRef PUBLIC_API CreateClassInstanceType(GlobalTSTypeRef gt);
 
+    GlobalTSTypeRef PUBLIC_API GetClassType(GlobalTSTypeRef classInstanceGT) const;
+
     GlobalTSTypeRef PUBLIC_API GetPropType(GlobalTSTypeRef gt, const uint64_t key) const;
 
     uint32_t PUBLIC_API GetUnionTypeLength(GlobalTSTypeRef gt) const;
@@ -172,9 +174,11 @@ public:
 
     GlobalTSTypeRef PUBLIC_API GetOrCreateUnionType(CVector<GlobalTSTypeRef> unionTypeVec);
 
-    int PUBLIC_API GetFunctionTypLength(GlobalTSTypeRef gt) const;
+    uint32_t PUBLIC_API GetFunctionTypeLength(GlobalTSTypeRef gt) const;
 
     GlobalTSTypeRef PUBLIC_API GetFuncParameterTypeGT(GlobalTSTypeRef gt, int index) const;
+
+    GlobalTSTypeRef PUBLIC_API GetFuncThisGT(GlobalTSTypeRef gt) const;
 
     inline GlobalTSTypeRef PUBLIC_API GetFuncReturnValueTypeGT(kungfu::GateType gateType) const
     {
