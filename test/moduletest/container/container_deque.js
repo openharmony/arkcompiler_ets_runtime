88d677d2c9f162f90fe921bfb81dc0a1192f58d8
wupengyong <wupengyong@huawei.com>
2022-10-28 15:58:15 +0800

    reason:fix containers ir description:fix containers ir issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I5Y9QG?from=project-issue
    Signed-off-by: wupengyong <wupengyong@huawei.com>
    Change-Id: I5b2a53bdeff7e7eedcc16bc04230c2b51247a5e2

--- test/moduletest/container/container_deque.js
+++ test/moduletest/container/container_deque.js
@@ -105,6 +105,12 @@ if (globalThis["ArkPrivate"] != undefined) {
         }
     }
     map.forEach(elements);
+    let de = new Deque();
+    try {
+        de.forEach(123);
+    } catch(err) {
+        print(err.name);
+    }
     if (!flag) {
         print("Test Deque success!!!");
     } else {
