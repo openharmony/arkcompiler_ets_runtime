5c746e6f35218c9dc2b5d1792843e96efe548cad
weng-xi <wengxi1@huawei.com>
2022-11-18 18:48:25 +0800

    Refactor bytecode_info_collector and aot_snapshot
    1. Remove useless code about old ISA in bytecode_info_collector
    2. Change the location of snapshot-info-collection from ts_manager to bytecode_info_collector
    3. Simplify the code logic of pass_manager
    4. Fix pdfjs benchmark
    
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I61Z1X
    Signed-off-by: weng-xi <wengxi1@huawei.com>
    Change-Id: I0d5dbc7215b63f54a807e0ae52c6cad5e606566d

--- ecmascript/compiler/bytecode_info_collector.h
+++ ecmascript/compiler/bytecode_info_collector.h
@@ -16,16 +16,13 @@
 #ifndef ECMASCRIPT_COMPILER_BYTECODE_INFO_COLLECTOR_H
 #define ECMASCRIPT_COMPILER_BYTECODE_INFO_COLLECTOR_H
 
+#include "ecmascript/dfx/pgo_profiler/pgo_profiler_loader.h"
 #include "ecmascript/jspandafile/js_pandafile.h"
 #include "ecmascript/jspandafile/method_literal.h"
 #include "ecmascript/jspandafile/bytecode_inst/old_instruction.h"
 #include "libpandafile/bytecode_instruction-inl.h"
 
 namespace panda::ecmascript::kungfu {
-
-// need to remove in the future
-enum FixInsIndex : uint8_t { FIX_ONE = 1, FIX_TWO = 2, FIX_FOUR = 4 };
-
 /*    ts source code
  *    let a:number = 1;
  *    function f() {
@@ -98,24 +95,143 @@ struct MethodInfo {
     LexEnv lexEnv;
 };
 
-struct BCInfo {
-    std::vector<uint32_t> mainMethodIndexes {};
-    std::vector<CString> recordNames {};
-    std::vector<MethodPcInfo> methodPcInfos {};
-    std::unordered_map<uint32_t, MethodInfo> methodList {};
+enum class ConstantPoolIndexType : uint8_t {
+    STRING,
+    METHOD,
+    CLASS_LITERAL,
+    OBJECT_LITERAL,
+    ARRAY_LITERAL,
+};
+
+class ConstantPoolIndexInfo {
+public:
+    const std::set<uint32_t>& GetStringOrMethodIndexSet(ConstantPoolIndexType type)
+    {
+        switch (type) {
+            case ConstantPoolIndexType::STRING: {
+                return stringIndex_;
+            }
+            case ConstantPoolIndexType::METHOD: {
+                return methodIndex_;
+            }
+            default:
+                UNREACHABLE();
+        }
+    }
+
+    const std::set<std::pair<uint32_t, uint32_t>>& GetLiteralIndexSet(ConstantPoolIndexType type)
+    {
+        switch (type) {
+            case ConstantPoolIndexType::CLASS_LITERAL: {
+                return classLiteralIndex_;
+            }
+            case ConstantPoolIndexType::OBJECT_LITERAL: {
+                return objectLiteralIndex_;
+            }
+            case ConstantPoolIndexType::ARRAY_LITERAL: {
+                return arrayLiteralIndex_;
+            }
+            default:
+                UNREACHABLE();
+        }
+    }
+
+    void AddConstantPoolIndex(ConstantPoolIndexType type, uint32_t index, uint32_t methodOffset = 0);
+
+private:
+    std::set<uint32_t> stringIndex_ {};
+    std::set<uint32_t> methodIndex_ {};
+
+    // literal need to record methodOffset (constantpool index, methodOffset)
+    std::set<std::pair<uint32_t, uint32_t>> classLiteralIndex_ {};
+    std::set<std::pair<uint32_t, uint32_t>> objectLiteralIndex_ {};
+    std::set<std::pair<uint32_t, uint32_t>> arrayLiteralIndex_ {};
+};
+
+class BCInfo {
+public:
+    explicit BCInfo(PGOProfilerLoader &profilerLoader, size_t maxAotMethodSize)
+        : pfLoader_(profilerLoader), maxMethodSize_(maxAotMethodSize)
+    {
+    }
+
+    std::vector<uint32_t>& GetMainMethodIndexes()
+    {
+        return mainMethodIndexes_;
+    }
+
+    std::vector<CString>& GetRecordNames()
+    {
+        return recordNames_;
+    }
+
+    std::vector<MethodPcInfo>& GetMethodPcInfos()
+    {
+        return methodPcInfos_;
+    }
+
+    std::unordered_map<uint32_t, MethodInfo>& GetMethodList()
+    {
+        return methodList_;
+    }
+
+    bool IsSkippedMethod(uint32_t methodOffset) const
+    {
+        if (skippedMethods_.find(methodOffset) == skippedMethods_.end()) {
+            return false;
+        }
+        return true;
+    }
+
+    size_t GetSkippedMethodSize() const
+    {
+        return skippedMethods_.size();
+    }
+
+    void AddConstantPoolIndex(ConstantPoolIndexType type, uint32_t index, uint32_t methodOffset = 0)
+    {
+        cpIndexInfo_.AddConstantPoolIndex(type, index, methodOffset);
+    }
 
     template <class Callback>
-    void EnumerateBCInfo(const Callback &cb)
+    void IterateStringOrMethodIndex(ConstantPoolIndexType type, const Callback &cb)
     {
-        for (uint32_t i = 0; i < mainMethodIndexes.size(); i++) {
+        const auto &indexSet = cpIndexInfo_.GetStringOrMethodIndexSet(type);
+        for (uint32_t index : indexSet) {
+            cb(index);
+        }
+    }
+
+    template <class Callback>
+    void IterateLiteralIndex(ConstantPoolIndexType type, const Callback &cb)
+    {
+        const auto &indexSet = cpIndexInfo_.GetLiteralIndexSet(type);
+        for (const auto &item : indexSet) {
+            cb(item.first, methodOffsetToRecordName_[item.second]);
+        }
+    }
+
+    template <class Callback>
+    void EnumerateBCInfo(JSPandaFile *jsPandaFile, const Callback &cb)
+    {
+        for (uint32_t i = 0; i < mainMethodIndexes_.size(); i++) {
             std::queue<uint32_t> methodCompiledOrder;
-            methodCompiledOrder.push(mainMethodIndexes[i]);
+            methodCompiledOrder.push(mainMethodIndexes_[i]);
             while (!methodCompiledOrder.empty()) {
                 auto compilingMethod = methodCompiledOrder.front();
                 methodCompiledOrder.pop();
-                auto &methodInfo = methodList.at(compilingMethod);
-                auto &methodPcInfo = methodPcInfos[methodInfo.methodPcInfoIndex];
-                cb(recordNames[i], compilingMethod, methodPcInfo, methodInfo.methodInfoIndex);
+                methodOffsetToRecordName_.emplace(compilingMethod, recordNames_[i]);
+                auto &methodInfo = methodList_.at(compilingMethod);
+                auto &methodPcInfo = methodPcInfos_[methodInfo.methodPcInfoIndex];
+                auto methodLiteral = jsPandaFile->FindMethodLiteral(compilingMethod);
+                const std::string methodName(MethodLiteral::GetMethodName(jsPandaFile, methodLiteral->GetMethodId()));
+                if (FilterMethod(recordNames_[i], methodLiteral, methodPcInfo)) {
+                    skippedMethods_.insert(compilingMethod);
+                    LOG_COMPILER(INFO) << " method " << methodName << " has been skipped";
+                } else {
+                    cb(recordNames_[i], methodName, methodLiteral, compilingMethod,
+                       methodPcInfo, methodInfo.methodInfoIndex);
+                }
                 auto &innerMethods = methodInfo.innerMethods;
                 for (auto it : innerMethods) {
                     methodCompiledOrder.push(it);
@@ -123,6 +239,26 @@ struct BCInfo {
             }
         }
     }
+private:
+    bool FilterMethod(const CString &recordName, const MethodLiteral *methodLiteral,
+                      const MethodPcInfo &methodPCInfo) const
+    {
+        if (methodPCInfo.methodsSize > maxMethodSize_ ||
+            !pfLoader_.Match(recordName, methodLiteral->GetMethodId())) {
+            return true;
+        }
+        return false;
+    }
+
+    std::vector<uint32_t> mainMethodIndexes_ {};
+    std::vector<CString> recordNames_ {};
+    std::vector<MethodPcInfo> methodPcInfos_ {};
+    std::unordered_map<uint32_t, MethodInfo> methodList_ {};
+    std::unordered_map<uint32_t, CString> methodOffsetToRecordName_ {};
+    std::set<uint32_t> skippedMethods_ {};
+    ConstantPoolIndexInfo cpIndexInfo_;
+    PGOProfilerLoader &pfLoader_;
+    size_t maxMethodSize_;
 };
 
 class LexEnvManager {
@@ -143,7 +279,9 @@ private:
 
 class BytecodeInfoCollector {
 public:
-    explicit BytecodeInfoCollector(JSPandaFile *jsPandaFile) : jsPandaFile_(jsPandaFile)
+    explicit BytecodeInfoCollector(JSPandaFile *jsPandaFile, PGOProfilerLoader &profilerLoader,
+                                   size_t maxAotMethodSize)
+        : jsPandaFile_(jsPandaFile), bytecodeInfo_(profilerLoader, maxAotMethodSize)
     {
         ProcessClasses();
     }
@@ -151,14 +289,26 @@ public:
     NO_COPY_SEMANTIC(BytecodeInfoCollector);
     NO_MOVE_SEMANTIC(BytecodeInfoCollector);
 
-    const JSPandaFile *GetJsPandaFile() const
-    {
-        return jsPandaFile_;
-    }
-
     BCInfo &GetBytecodeInfo()
     {
         return bytecodeInfo_;
+    }
+
+    bool IsSkippedMethod(uint32_t methodOffset) const
+    {
+        return bytecodeInfo_.IsSkippedMethod(methodOffset);
+    }
+
+    template <class Callback>
+    void IterateStringOrMethodIndex(ConstantPoolIndexType type, const Callback &cb)
+    {
+        bytecodeInfo_.IterateStringOrMethodIndex(type, cb);
+    }
+
+    template <class Callback>
+    void IterateLiteralIndex(ConstantPoolIndexType type, const Callback &cb)
+    {
+        bytecodeInfo_.IterateLiteralIndex(type, cb);
     }
 
 private:
@@ -167,35 +317,23 @@ private:
         return methodInfoIndex_++;
     }
 
+    void AddConstantPoolIndexToBCInfo(ConstantPoolIndexType type, uint32_t index, uint32_t methodOffset = 0)
+    {
+        bytecodeInfo_.AddConstantPoolIndex(type, index, methodOffset);
+    }
+
     const CString GetEntryFunName(const std::string_view &entryPoint) const;
     void ProcessClasses();
-    void CollectMethodPcs(const uint32_t insSz, const uint8_t *insArr, const MethodLiteral *method,
-                          const CString &entryPoint = "func_main_0");
-    void CollectMethodPcsFromNewBc(const uint32_t insSz, const uint8_t *insArr, const MethodLiteral *method);
+    void CollectMethodPcsFromBC(const uint32_t insSz, const uint8_t *insArr, const MethodLiteral *method);
     void SetMethodPcInfoIndex(uint32_t methodOffset, size_t index);
     void CollectInnerMethods(const MethodLiteral *method, uint32_t innerMethodOffset);
     void CollectInnerMethods(uint32_t methodId, uint32_t innerMethodOffset);
     void CollectInnerMethodsFromLiteral(const MethodLiteral *method, uint64_t index);
     void NewLexEnvWithSize(const MethodLiteral *method, uint64_t numOfLexVars);
 
-    static void AddNopInst(uint8_t *pc, int number);
-
-    // need to remove in the future
-    static void FixOpcode(MethodLiteral *method, const OldBytecodeInst &inst);
-    static void FixOpcode(const OldBytecodeInst &inst);
-
-    // need to remove in the future
-    static void UpdateICOffset(MethodLiteral *method, uint8_t *pc);
-
-    // need to remove in the future
-    static void FixInstructionId32(const OldBytecodeInst &inst, uint32_t index, uint32_t fixOrder = 0);
-
-    // need to remove in the future
-    void TranslateBCIns(const OldBytecodeInst &bcIns, const MethodLiteral *method, const CString &entryPoint);
-
-    // use for new ISA
     void CollectInnerMethodsFromNewLiteral(const MethodLiteral *method, panda_file::File::EntityId literalId);
-    void CollectMethodInfoFromNewBC(const BytecodeInstruction &bcIns, const MethodLiteral *method);
+    void CollectMethodInfoFromBC(const BytecodeInstruction &bcIns, const MethodLiteral *method);
+    void CollectConstantPoolIndexInfoFromBC(const BytecodeInstruction &bcIns, const MethodLiteral *method);
 
     JSPandaFile *jsPandaFile_ {nullptr};
     BCInfo bytecodeInfo_;
