c3ceecd507141f7e048ae1aacc275041d2b108da
g00416891 <guobingbing3@huawei.com>
2022-11-26 15:24:12 +0800

    Fix code check
    Signed-off-by: g00416891 <guobingbing3@huawei.com>
    Change-Id: I6432ff57276e0671b985cb7efa838253e9864c73

--- ecmascript/frames.cpp
+++ ecmascript/frames.cpp
@@ -296,7 +296,7 @@ uintptr_t FrameIterator::GetPrevFrameCallSiteSp([[maybe_unused]] uintptr_t curPc
             return 0;
         }
         default: {
-            LOG_ECMA(FATAL) << "frame type error!";
+            LOG_FULL(FATAL) << "frame type error!";
         }
     }
 }
@@ -323,7 +323,7 @@ uintptr_t FrameIterator::GetPrevFrame() const
             break;
         }
         default: {
-            LOG_ECMA(FATAL) << "frame type error!";
+            LOG_FULL(FATAL) << "frame type error!";
         }
     }
     return end;
