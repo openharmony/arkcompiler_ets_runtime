15f5fdc43bcff18b65ae899aea88df6a764d290a
yuan-yao14 <yuanyao14@huawei.com>
2022-12-12 19:58:55 +0800

    New an instance of MemMapAllocator
    Description:
    	New an instance of MemMapAllocator which prevents it being freed before final usage.
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I65VLO
    
    Signed-off-by: yuan-yao14 <yuanyao14@huawei.com>
    Change-Id: I604cce3a0c89c3aee5cc1ea08602786c12c5ed8c

--- ecmascript/mem/mem_map_allocator.h
+++ ecmascript/mem/mem_map_allocator.h
@@ -246,8 +246,8 @@ public:
 
     static MemMapAllocator *GetInstance()
     {
-        static MemMapAllocator vmAllocator_;
-        return &vmAllocator_;
+        static MemMapAllocator *vmAllocator_ = new MemMapAllocator();
+        return vmAllocator_;
     }
 
     MemMap Allocate(size_t size, size_t alignment, bool regular, int prot);
