e1af85e9a17bf45e7e98944c704c18f86542d6cd
Gymee <yumeijie@huawei.com>
2022-12-14 17:56:59 +0800

    Optimize and refactor StringHashMap
    Issue: #I66MGF
    Signed-off-by: Gymee <yumeijie@huawei.com>
    Change-Id: I2d9406025c0e874d1d5b6b667f137481abb33b3e

--- ecmascript/dfx/hprof/string_hashmap.cpp
+++ ecmascript/dfx/hprof/string_hashmap.cpp
@@ -18,24 +18,26 @@
 #include "ecmascript/mem/native_area_allocator.h"
 
 namespace panda::ecmascript {
-CString *StringHashMap::FindOrInsertString(CString *string)
+CString *StringHashMap::FindOrInsertString(const CString *cstr)
 {
-    StringKey key = GenerateStringKey(string);
+    StringKey key = GenerateStringKey(cstr);
     auto it = hashmap_.find(key);
     if (it != hashmap_.end()) {
         return it->second;
     } else {  // NOLINT(readability-else-after-return)
         index_++;
-        hashmap_.emplace(key, string);
+        auto *newStr = const_cast<NativeAreaAllocator *>(
+            vm_->GetNativeAreaAllocator())->New<CString>(cstr->c_str());
+        hashmap_.emplace(key, newStr);
         orderedKey_.emplace_back(key);
         indexMap_.emplace(key, index_);
-        return string;
+        return newStr;
     }
 }
 
-StringId StringHashMap::GetStringId(const CString *string) const
+StringId StringHashMap::GetStringId(const CString *cstr) const
 {
-    auto it = indexMap_.find(GenerateStringKey(string));
+    auto it = indexMap_.find(GenerateStringKey(cstr));
     return it != indexMap_.end() ? it->second : 1;  // ""
 }
 
@@ -48,11 +50,11 @@ CString *StringHashMap::GetStringByKey(StringKey key) const
     return nullptr;
 }
 
-CString *StringHashMap::FormatString(CString *string) const
+CString *StringHashMap::FormatString(CString *cstr) const
 {
     // remove "\"" | "\r\n" | "\\" | "\t" | "\f"
-    int length = static_cast<int>(string->length());
-    char *charSeq = const_cast<char *>(string->c_str());
+    int length = static_cast<int>(cstr->length());
+    char *charSeq = const_cast<char *>(cstr->c_str());
     for (int i = 0; i < length; i++) {
         if (charSeq[i] == '\"') {         // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
             charSeq[i] = '`';             // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
@@ -71,31 +73,26 @@ CString *StringHashMap::FormatString(CString *string) const
             charSeq[i] = '`';  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
         }
     }
-    *string = charSeq;
-    return string;
+    *cstr = charSeq;
+    return cstr;
 }
 
-StringKey StringHashMap::GenerateStringKey(const CString *string) const
+StringKey StringHashMap::GenerateStringKey(const CString *cstr) const
 {
-    return std::hash<std::string>{} (std::string(*string));
+    return std::hash<CString>{} (*cstr);
 }
 
-CString *StringHashMap::GetString(CString as)
+CString *StringHashMap::GetString(const CString &cstr)
 {
-    auto *tempString = const_cast<NativeAreaAllocator *>(vm_->GetNativeAreaAllocator())->New<CString>(std::move(as));
-    CString *oldString = FindOrInsertString(tempString);
-    if (tempString != oldString) {
-        const_cast<NativeAreaAllocator *>(vm_->GetNativeAreaAllocator())->Delete(tempString);
-        return oldString;
-    }
-    return tempString;
+    return FindOrInsertString(&cstr);
 }
 
 void StringHashMap::Clear()
 {
-    for (auto it : hashmap_) {
-        if (it.second != nullptr) {
-            const_cast<NativeAreaAllocator *>(vm_->GetNativeAreaAllocator())->Delete(it.second);
+    auto *alloctor = const_cast<NativeAreaAllocator *>(vm_->GetNativeAreaAllocator());
+    for (auto &[_, cstr] : hashmap_) {
+        if (cstr != nullptr) {
+            alloctor->Delete(cstr);
         }
     }
 }
