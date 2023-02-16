ffcefc66cce8f30dcfbdde6b4a639315a1f0483c
Gymee <yumeijie@huawei.com>
2023-02-16 22:22:23 +0800

    Fix debugger for taskpool task
    Issue: #I6FORV
    Signed-off-by: Gymee <yumeijie@huawei.com>
    Change-Id: Idd472a5e807867225f5aa171cc5d0479c343b6e4

--- ecmascript/napi/jsnapi.cpp
+++ ecmascript/napi/jsnapi.cpp
@@ -2723,6 +2723,10 @@ bool JSNApi::InitForConcurrentFunction(EcmaVM *vm, Local<JSValueRef> function)
     ecmascript::CString moduleName = jsPandaFile->GetJSPandaFileDesc();
     ecmascript::CString recordName = method->GetRecordName();
 
+    // for debugger, to notify the script loaded and parsed which the concurrent function is in
+    auto *notificationMgr = vm->GetJsDebuggerManager()->GetNotificationManager();
+    notificationMgr->LoadModuleEvent(moduleName, recordName);
+
     bool isModule = jsPandaFile->IsModule(thread, recordName);
     if (isModule) {
         ecmascript::ModuleManager *moduleManager = vm->GetModuleManager();
