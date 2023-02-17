4f98c5c27741c380390235f30826098b07328bc8
zhangyukun8 <zhangyukun8@huawei.com>
2023-02-17 16:34:20 +0800

    Fix mmap failed with error code 22
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I6FVSI?from=project-issue
    Signed-off-by: zhangyukun8 <zhangyukun8@huawei.com>
    Change-Id: I70e6ea8432d7da112908282c2fa1c1cd0692fcd2

--- ecmascript/aot_file_manager.cpp
+++ ecmascript/aot_file_manager.cpp
@@ -162,6 +162,10 @@ void StubFileInfo::Save(const std::string &filename)
         return;
     }
 
+    if (totalCodeSize_ == 0) {
+        LOG_COMPILER(FATAL) << "error: code size of generated stubs is empty!";
+    }
+
     std::ofstream file(realPath.c_str(), std::ofstream::binary);
     SetStubNum(entries_.size());
     file.write(reinterpret_cast<char *>(&entryNum_), sizeof(entryNum_));
@@ -193,6 +197,11 @@ bool StubFileInfo::Load()
     binBufparser.ParseBuffer(&moduleNum_, sizeof(moduleNum_));
     des_.resize(moduleNum_);
     binBufparser.ParseBuffer(&totalCodeSize_, sizeof(totalCodeSize_));
+
+    if (totalCodeSize_ == 0) {
+        LOG_COMPILER(ERROR) << "error: code in the binary stub is empty!";
+        return false;
+    }
 
     ExecutedMemoryAllocator::AllocateBuf(totalCodeSize_, exeMem_);
     uint64_t codeAddress = reinterpret_cast<uint64_t>(exeMem_.addr_);
@@ -227,6 +236,11 @@ void AnFileInfo::Save(const std::string &filename, kungfu::Triple triple)
     if (!RealPath(filename, realPath, false)) {
         return;
     }
+
+    if (totalCodeSize_ == 0) {
+        LOG_COMPILER(FATAL) << "error: code size of generated an file is empty!";
+    }
+
     std::ofstream file(realPath.c_str(), std::ofstream::binary);
     SetStubNum(entries_.size());
 
@@ -311,6 +325,11 @@ bool AnFileInfo::Load(const std::string &filename)
     des_.resize(moduleNum_);
     file.read(reinterpret_cast<char *>(&totalCodeSize_), sizeof(totalCodeSize_));
 
+    if (totalCodeSize_ == 0) {
+        file.close();
+        LOG_COMPILER(ERROR) << "error: code in the an file is empty!";
+        return false;
+    }
     ExecutedMemoryAllocator::AllocateBuf(totalCodeSize_, exeMem_);
     uint64_t codeAddress = reinterpret_cast<uint64_t>(exeMem_.addr_);
     uint32_t curUnitOffset = 0;
