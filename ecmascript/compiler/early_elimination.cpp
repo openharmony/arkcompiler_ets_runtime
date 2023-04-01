31cc8ce0c78f4f09f8072dffb8c3127e7d3dfb8e
K0u1hw <wangchunyang15@huawei.com>
2023-04-01 14:32:28 +0800

    Description: cherry pick 3850 to 3.2 release Issues: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I6UNCR
    Signed-off-by: K0u1hw <wangchunyang15@huawei.com>
    Change-Id: Ibe56f248cd088038cf3910148002768155183346

--- ecmascript/compiler/early_elimination.cpp
+++ ecmascript/compiler/early_elimination.cpp
@@ -350,7 +350,6 @@ bool EarlyElimination::IsSideEffectLoop(GateRef depend)
     while (!workList.empty()) {
         auto curDep = workList.front();
         workList.pop();
-        if (!visited.empty()) return false;
         if (visited.count(curDep)) {
             continue;
         }
