fe4db014eced0b4a74840d68052659b8a10940f2
sunzhe23 <sunzhe23@huawei.com>
2023-04-18 21:01:22 +0800

    Add Value numbering to elimite Convert
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I6WTH8
    
    Signed-off-by: sunzhe23 <sunzhe23@huawei.com>

---
+++ ecmascript/compiler/value_numbering.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "ecmascript/compiler/value_numbering.h"
+
+namespace panda::ecmascript::kungfu {
+void ValueNumbering::Run()
+{
+    VisitGraph();
+    if (IsLogEnabled()) {
+        LOG_COMPILER(INFO) << "";
+        LOG_COMPILER(INFO) << "\033[34m"
+                           << "===================="
+                           << " After value numbering "
+                           << "[" << GetMethodName() << "]"
+                           << "===================="
+                           << "\033[0m";
+        circuit_->PrintAllGatesWithBytecode();
+        LOG_COMPILER(INFO) << "\033[34m" << "========================= End ==========================" << "\033[0m";
+    }
+}
+
+GateRef ValueNumbering::VisitGate(GateRef gate)
+{
+    auto opcode = acc_.GetOpCode(gate);
+    if (opcode != OpCode::CONVERT) {
+        return Circuit::NullGate();
+    }
+    size_t hash = HashCode(gate);
+    if (entries_.size() == 0) {
+        entries_.resize(CACHE_LENGTH, Circuit::NullGate());
+        SetEntry(hash, gate);
+        return Circuit::NullGate();
+    }
+    GateRef replacement = GetEntry(hash);
+    if (replacement != Circuit::NullGate() &&
+        CheckReplacement(gate, replacement)) {
+        return replacement;
+    }
+    SetEntry(hash, gate);
+    return Circuit::NullGate();
+}
+
+size_t ValueNumbering::HashCode(GateRef gate)
+{
+    size_t hash = static_cast<size_t>(acc_.GetOpCode(gate));
+    hash ^= acc_.TryGetValue(gate);
+    size_t valueCount = acc_.GetNumValueIn(gate);
+    for (size_t i = 0; i < valueCount; i++) {
+        GateRef input = acc_.GetValueIn(gate);
+        auto id = acc_.GetId(input);
+        hash ^= id;
+    }
+    return hash % CACHE_LENGTH;
+}
+
+bool ValueNumbering::CheckReplacement(GateRef lhs, GateRef rhs)
+{
+    if (acc_.GetMetaData(lhs) != acc_.GetMetaData(rhs)) {
+        if (acc_.GetOpCode(lhs) != acc_.GetOpCode(rhs)) {
+            return false;
+        }
+    }
+    size_t valueCount = acc_.GetNumValueIn(lhs);
+    for (size_t i = 0; i < valueCount; i++) {
+        if (acc_.GetValueIn(lhs, i) != acc_.GetValueIn(rhs, i)) {
+            return false;
+        }
+    }
+    auto opcode = acc_.GetOpCode(lhs);
+    if (opcode == OpCode::CONVERT) {
+        if (acc_.GetSrcType(lhs) != acc_.GetSrcType(rhs)) {
+            return false;
+        }
+        if (acc_.GetDstType(lhs) != acc_.GetDstType(rhs)) {
+            return false;
+        }
+    }
+    return true;
+}
+}  // namespace panda::ecmascript::kungfu
