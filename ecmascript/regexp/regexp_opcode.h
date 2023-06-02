f78611ef4847578298b4dd019702ae86524f3116
王笑佳 <wangxiaojia5@huawei.com>
2023-06-02 11:59:44 +0800

    Revert RegExp bugFix
    issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I7AC8E
    
    Signed-off-by: 王笑佳 <wangxiaojia5@huawei.com>

--- ecmascript/regexp/regexp_opcode.h
+++ ecmascript/regexp/regexp_opcode.h
@@ -359,7 +359,6 @@ public:
     void Insert(uint32_t start, uint32_t end);
     void Insert(const RangeSet &s1);
     void Invert(bool isUtf16);
-    void Inter(RangeSet &cr, const RangeSet &s1);
     void Compress();
 
 private:
