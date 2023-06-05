b7d303ec34e7bc3040c5b6949e5de37adb1a3523
zhangyouyou <zhangyouyou2@huawei.com>
2023-06-05 10:12:43 +0800

    fix ut bug
    Signed-off-by: zhangyouyou <zhangyouyou2@huawei.com>
    Change-Id: I8ee8c6e0e46d0175f9b4b0c1588311737d6c23b3

--- ecmascript/tests/ecma_context_test.cpp
+++ ecmascript/tests/ecma_context_test.cpp
@@ -53,11 +53,14 @@ public:
 HWTEST_F_L0(EcmaContextTest, Create)
 {
     [[maybe_unused]]auto context = EcmaContext::Create(thread);
-    CVector<EcmaContext *> context3 = thread->GetEcmaContexts();
-    EXPECT_EQ(context3.size(), 1);
-    [[maybe_unused]]auto context2 = EcmaContext::MountContext(thread);
-    EXPECT_EQ(context3.size(), 2);
-
+    CVector<EcmaContext *> Cv1 = thread->GetEcmaContexts();
+    EXPECT_EQ(Cv1.size(), 1);
+    auto context2 = EcmaContext::Create(thread);
+    Cv1 = thread->GetEcmaContexts();
+    EXPECT_EQ(Cv1.size(), 1);
+    thread->PushContext(context2);
+    Cv1 = thread->GetEcmaContexts();
+    EXPECT_EQ(Cv1.size(), 2);
 }
 
 HWTEST_F_L0(EcmaContextTest, GetEcmaStringTable)
@@ -73,23 +76,16 @@ HWTEST_F_L0(EcmaContextTest, GetEcmaStringTable)
 
 HWTEST_F_L0(EcmaContextTest, CreatePushContext)
 {
-    EcmaVM *vm = thread->GetEcmaVM();
-    ObjectFactory *factory = vm->GetFactory();
     auto context = EcmaContext::Create(thread);
     auto context1 = EcmaContext::Create(thread);
-    EcmaStringTable *table1 = context->GetEcmaStringTable();
-    JSHandle<EcmaString> ecmaStrCreateHandle1 = factory->NewFromASCII("hello world");
-    EcmaString *ecmaStrGetPtr1 = table1->GetOrInternString(*ecmaStrCreateHandle1);
-    EXPECT_STREQ(EcmaStringAccessor(ecmaStrGetPtr1).ToCString().c_str(), "hello world");
-    EcmaStringTable *table2 = context1->GetEcmaStringTable();
-    JSHandle<EcmaString> ecmaStrCreateHandle2 = factory->NewFromASCII("hello world2");
-    EcmaString *ecmaStrGetPtr2 = table2->GetOrInternString(*ecmaStrCreateHandle2);
-    EXPECT_STREQ(EcmaStringAccessor(ecmaStrGetPtr2).ToCString().c_str(), "hello world2");
+    thread->PushContext(context1);
     CVector<EcmaContext *> context3 = thread->GetEcmaContexts();
     EXPECT_EQ(context3.size(), 2);
     thread->PushContext(context);
+    context3 = thread->GetEcmaContexts();
     EXPECT_EQ(context3.size(), 3);
     thread->PopContext();
+    context3 = thread->GetEcmaContexts();
     EXPECT_EQ(context3.size(), 2);   
 }
 
@@ -98,9 +94,6 @@ HWTEST_F_L0(EcmaContextTest, GetRegExpCache)
     EcmaVM *vm = thread->GetEcmaVM();
     ObjectFactory *factory = vm->GetFactory();
     auto context = EcmaContext::Create(thread);
-    JSHandle<JSTaggedValue> value = context->GetRegExpCache();
-    JSTaggedValue res = value.GetTaggedValue();
-    EXPECT_EQ(res, JSTaggedValue::Null());
     JSHandle<EcmaString> regexp = factory->NewFromASCII("\\g");
     JSHandle<JSTaggedValue> value2(regexp);
     context->SetRegExpCache(value2.GetTaggedValue());
@@ -117,23 +110,5 @@ HWTEST_F_L0(EcmaContextTest, AllowAtomicWait)
     bool value2 = context->GetAllowAtomicWait();
     EXPECT_FALSE(value2);
 }
-
-HWTEST_F_L0(EcmaContextTest, SwitchCurrentContext)
-{
-    auto context1 = EcmaContext::Create(thread);
-    context1->SetAllowAtomicWait(false);
-    bool value1 = thread->GetCurrentEcmaContext()->GetAllowAtomicWait();
-    EXPECT_FALSE(value1);
-    auto context2 = EcmaContext::Create(thread);
-    bool wait = context2->GetAllowAtomicWait();
-    EXPECT_TRUE(wait);
-    thread->SwitchCurrentContext(context2);
-    bool value2 = thread->GetCurrentEcmaContext()->GetAllowAtomicWait();
-    EXPECT_TRUE(value2);
-    thread->SwitchCurrentContext(context1);
-    bool value3 = thread->GetCurrentEcmaContext()->GetAllowAtomicWait();
-    EXPECT_FALSE(value3);
-}
-
 
 }  // namespace panda::test
