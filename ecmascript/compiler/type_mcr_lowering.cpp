81811ab7f8ec21ac17539d147488a68b61eb8a7b
DDOSvoid <DDOSvoid@163.com>
2023-07-20 11:45:00 +0800

    Erase load byteOffset in TypedArray load/store and add aottest case issues: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I7MJBU Signed-off-by: DDOSvoid <DDOSvoid@163.com>
    Change-Id: I3f8abde85e55cb9f82749c74fbe3f6b5444785f9

--- ecmascript/compiler/type_mcr_lowering.cpp
+++ ecmascript/compiler/type_mcr_lowering.cpp
@@ -696,12 +696,10 @@ void TypeMCRLowering::LowerTypedArrayLoadElement(GateRef gate, BuiltinTypeId id)
     GateRef index = acc_.GetValueIn(gate, 1);
     GateRef elementSize = GetElementSize(id);
     GateRef offset = builder_.PtrMul(index, elementSize);
-    GateRef byteOffset =
-        builder_.LoadConstOffset(VariableType::INT32(), receiver, JSTypedArray::BYTE_OFFSET_OFFSET);
 
     GateRef data = builder_.PtrAdd(arrbuffer, builder_.IntPtr(ByteArray::DATA_OFFSET));
     auto type = GetVariableType(id);
-    GateRef result = builder_.Load(type, data, builder_.PtrAdd(offset, byteOffset));
+    GateRef result = builder_.Load(type, data, offset);
     switch (id) {
         case BuiltinTypeId::INT8_ARRAY:
             result = builder_.SExtInt8ToInt32(result);
@@ -801,7 +799,6 @@ void TypeMCRLowering::LowerTypedArrayStoreElement(GateRef gate, BuiltinTypeId id
 
     GateRef elementSize = GetElementSize(id);
     GateRef offset = builder_.PtrMul(index, elementSize);
-    GateRef byteOffset = builder_.LoadConstOffset(VariableType::INT32(), receiver, JSTypedArray::BYTE_OFFSET_OFFSET);
     switch (id) {
         case BuiltinTypeId::INT8_ARRAY:
         case BuiltinTypeId::UINT8_ARRAY:
@@ -820,7 +817,7 @@ void TypeMCRLowering::LowerTypedArrayStoreElement(GateRef gate, BuiltinTypeId id
     GateRef arrbuffer = builder_.LoadConstOffset(VariableType::JS_POINTER(), receiver, JSTypedArray::VIEWED_ARRAY_BUFFER_OFFSET);
     GateRef data = builder_.PtrAdd(arrbuffer, builder_.IntPtr(ByteArray::DATA_OFFSET));
 
-    builder_.StoreMemory(MemoryType::ELEMENT_TYPE, VariableType::VOID(), data, builder_.PtrAdd(offset, byteOffset), value);
+    builder_.StoreMemory(MemoryType::ELEMENT_TYPE, VariableType::VOID(), data, offset, value);
     acc_.ReplaceGate(gate, builder_.GetState(), builder_.GetDepend(), Circuit::NullGate());
 }
 
@@ -833,8 +830,6 @@ void TypeMCRLowering::LowerUInt8ClampedArrayStoreElement(GateRef gate)
     GateRef index = acc_.GetValueIn(gate, 1);
     GateRef elementSize = builder_.Int32(sizeof(uint8_t));
     GateRef offset = builder_.PtrMul(index, elementSize);
-    GateRef byteOffset =
-        builder_.LoadConstOffset(VariableType::INT32(), receiver, JSTypedArray::BYTE_OFFSET_OFFSET);
     GateRef value = acc_.GetValueIn(gate, 2);
 
     DEFVAlUE(result, (&builder_), VariableType::INT32(), value);
@@ -866,7 +861,7 @@ void TypeMCRLowering::LowerUInt8ClampedArrayStoreElement(GateRef gate)
     
     GateRef data = builder_.PtrAdd(arrbuffer, builder_.IntPtr(ByteArray::DATA_OFFSET));
 
-    builder_.StoreMemory(MemoryType::ELEMENT_TYPE, VariableType::VOID(), data, builder_.PtrAdd(offset, byteOffset), value);
+    builder_.StoreMemory(MemoryType::ELEMENT_TYPE, VariableType::VOID(), data, offset, value);
     acc_.ReplaceGate(gate, builder_.GetState(), builder_.GetDepend(), Circuit::NullGate());
 }
 
