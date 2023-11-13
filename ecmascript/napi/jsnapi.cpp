2ef80818364a6096e4e087c8fc43dedf33588732
dingding <dingding5@huawei.com>
2023-11-13 10:06:12 +0800

    Add Thread Check in JSNApi::DisposeGlobalHandleAddr()
    There is a beta problem and we suspect it is a multi-threading issue. Add forced multi-thread check in JSNApi::DisposeGlobalHandleAddr() for observation in the next version.
    
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I8FVUF
    
    Signed-off-by: dingding <dingding5@huawei.com>
    Change-Id: I416290dd1a1781da15aeae0526f0450bb39b1b96

--- ecmascript/napi/jsnapi.cpp
+++ ecmascript/napi/jsnapi.cpp
@@ -808,6 +808,7 @@ void JSNApi::DisposeGlobalHandleAddr(const EcmaVM *vm, uintptr_t addr)
     if (addr == 0 || !reinterpret_cast<ecmascript::Node *>(addr)->IsUsing()) {
         return;
     }
+    vm->CheckThread();
     vm->GetJSThread()->DisposeGlobalHandle(addr);
 }
 
