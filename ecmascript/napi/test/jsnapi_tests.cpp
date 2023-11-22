3708eefdfc2c8cdd64c952d24b7c57d539bceb50
lukai <lukai25@huawei.com>
2023-11-22 17:19:19 +0800

    Register ArkNativeFunctionCallBack directly
    Remove the middle-level call named "registerCallback" for napi.
    Issue:https://gitee.com/openharmony/arkui_napi/issues/I8IN1S?from=project-issue
    
    Signed-off-by: lukai <lukai25@huawei.com>
    Change-Id: Icc195388f09a84a3911c80bf30a87bcabc7830e9

--- ecmascript/napi/test/jsnapi_tests.cpp
+++ ecmascript/napi/test/jsnapi_tests.cpp
@@ -985,7 +985,7 @@ HWTEST_F_L0(JSNApiTests, InheritPrototype_004)
 HWTEST_F_L0(JSNApiTests, ClassFunction)
 {
     LocalScope scope(vm_);
-    Local<FunctionRef> cls = FunctionRef::NewClassFunction(vm_, nullptr, nullptr, nullptr);
+    Local<FunctionRef> cls = FunctionRef::NewClassFunction(vm_, FunctionCallback, nullptr, nullptr);
 
     JSHandle<JSTaggedValue> clsObj = JSNApiHelper::ToJSHandle(Local<JSValueRef>(cls));
     ASSERT_TRUE(clsObj->IsClassConstructor());
