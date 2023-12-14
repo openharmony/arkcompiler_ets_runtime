bfa8c8f15847521f6ef5999755e8135152f75a58
huangtianzhi <huangtianzhi1@huawei.com>
2023-12-14 09:27:43 +0800

    Debugger Adding Object Hash Info
    Adding hash info to objects' description
    
    Issue:#I8LT4V
    Test: Build & Boot & Debug
    
    Signed-off-by: huangtianzhi <huangtianzhi1@huawei.com>

--- ecmascript/debugger/debugger_api.cpp
+++ ecmascript/debugger/debugger_api.cpp
@@ -294,6 +294,12 @@ bool DebuggerApi::GetSingleStepStatus(JSDebugger *debugger)
     return debugger->GetSingleStepStatus();
 }
 
+int32_t DebuggerApi::GetObjectHash(const JSHandle<JSTaggedValue> &tagged) 
+{
+    bool hasHash = ECMAObject::Cast(tagged->GetTaggedObject())->HasHash();
+    return hasHash ? ECMAObject::Cast(tagged->GetTaggedObject())->GetHash() : 0;
+}
+
 // ScopeInfo
 Local<JSValueRef> DebuggerApi::GetProperties(const EcmaVM *ecmaVm, const FrameHandler *frameHandler,
                                              int32_t level, uint32_t slot)
