f83caa9b135d347077a575d9346223298bd93faa
xuqinghuo05 <xuqinghuo@h-partners.com>
2023-12-14 13:06:53 +0000

    update test/fuzztest/jsnapiexecute_fuzzer/jsnapiexecute_fuzzer.cpp.
    Signed-off-by: xuqinghuo05 <xuqinghuo@h-partners.com>

--- test/fuzztest/jsnapiexecute_fuzzer/jsnapiexecute_fuzzer.cpp
+++ test/fuzztest/jsnapiexecute_fuzzer/jsnapiexecute_fuzzer.cpp
@@ -28,7 +28,7 @@ void JSNApiExecuteFuzztest(const uint8_t *data, size_t size)
     RuntimeOption option;
     option.SetLogLevel(RuntimeOption::LOG_LEVEL::ERROR);
     EcmaVM *vm = JSNApi::CreateJSVM(option);
-    if (data == nullptr || size  <= 0) {
+    if (data == nullptr || size <= 0) {
         LOG_ECMA(ERROR) << "illegal input!";
         return;
     }
