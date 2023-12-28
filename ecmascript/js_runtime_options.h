697e8810dc04670f0c366699c712ab3d5e12962d
weng-xi <wengxi1@huawei.com>
2023-12-29 01:53:11 +0800

    Enable option for tryldglobalbyname
    Signed-off-by: weng-xi <wengxi1@huawei.com>
    Change-Id: I1a13e14dce7d902783aee4916b76bb31996c7201

--- ecmascript/js_runtime_options.h
+++ ecmascript/js_runtime_options.h
@@ -1537,7 +1537,7 @@ private:
     bool enableOptLoopInvariantCodeMotion_ {false};
     bool enableLexenvSpecialization_ {false};
     bool enableNativeInline_ {false};
-    bool enableLoweringBuiltin_ {false};
+    bool enableLoweringBuiltin_ {true};
     bool enableLiteCG_ {false};
     bool enableLoopHoistProfiler_ {false};
 };
