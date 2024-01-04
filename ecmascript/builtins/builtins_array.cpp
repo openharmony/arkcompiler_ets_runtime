a3a89714480b6e2ac1c18d1a326cbe8e1af727d1
yanpeng <yanpeng51@huawei.com>
2024-01-04 17:25:11 +0800

    fix arrayfill regress-crbug-1426357.js problem
    Signed-off-by: yanpeng <yanpeng51@huawei.com>

--- ecmascript/builtins/builtins_array.cpp
+++ ecmascript/builtins/builtins_array.cpp
@@ -774,8 +774,8 @@ JSTaggedValue BuiltinsArray::Fill(EcmaRuntimeCallInfo *argv)
     RETURN_EXCEPTION_IF_ABRUPT_COMPLETION(thread);
 
     // 5. Let relativeStart be ToInteger(start).
-    JSHandle<JSTaggedValue> msg1 = GetCallArg(argv, 1);
-    JSTaggedNumber argStartTemp = JSTaggedValue::ToInteger(thread, msg1);
+    JSHandle<JSTaggedValue> startArg = GetCallArg(argv, 1);
+    JSTaggedNumber argStartTemp = JSTaggedValue::ToInteger(thread, startArg);
     // 6. ReturnIfAbrupt(relativeStart).
     RETURN_EXCEPTION_IF_ABRUPT_COMPLETION(thread);
     double argStart = argStartTemp.GetNumber();
@@ -790,9 +790,9 @@ JSTaggedValue BuiltinsArray::Fill(EcmaRuntimeCallInfo *argv)
 
     // 8. If end is undefined, let relativeEnd be len; else let relativeEnd be ToInteger(end).
     double argEnd = len;
-    JSHandle<JSTaggedValue> msg2 = GetCallArg(argv, INDEX_TWO);
-    if (!msg2->IsUndefined()) {
-        JSTaggedNumber argEndTemp = JSTaggedValue::ToInteger(thread, msg2);
+    JSHandle<JSTaggedValue> endArg = GetCallArg(argv, INDEX_TWO);
+    if (!endArg->IsUndefined()) {
+        JSTaggedNumber argEndTemp = JSTaggedValue::ToInteger(thread, endArg);
         // 9. ReturnIfAbrupt(relativeEnd).
         RETURN_EXCEPTION_IF_ABRUPT_COMPLETION(thread);
         argEnd = argEndTemp.GetNumber();
@@ -811,8 +811,8 @@ JSTaggedValue BuiltinsArray::Fill(EcmaRuntimeCallInfo *argv)
     //   b. Let setStatus be Set(O, Pk, value, true).
     //   c. ReturnIfAbrupt(setStatus).
     //   d. Increase k by 1.
-    
-    if (thisObjVal->IsStableJSArray(thread)) {
+
+    if (thisObjVal->IsStableJSArray(thread) && !startArg->IsJSObject() && !endArg->IsJSObject()) {
         return JSStableArray::Fill(thread, thisObjHandle, value, start, end, len);
     }
 
