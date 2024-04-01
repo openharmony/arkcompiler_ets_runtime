c04428a62124acdbe55cd9a55b9df77a65285491
hecunmao <hecunmao@huawei.com>
2024-04-01 14:35:05 +0800

    [Bug]: NotifyHclassChanged is not triggered when hclass changes issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I9D8QT
    Signed-off-by: hecunmao <hecunmao@huawei.com>
    Change-Id: I51e5344cf5a507cac92bde0912b8bc5f405dfdd2

--- ecmascript/js_object.cpp
+++ ecmascript/js_object.cpp
@@ -1476,6 +1476,9 @@ bool JSObject::PreventExtensions(JSThread *thread, const JSHandle<JSObject> &obj
     if (obj->IsExtensible()) {
         JSHandle<JSHClass> jshclass(thread, obj->GetJSHClass());
         JSHandle<JSHClass> newHclass = JSHClass::TransitionExtension(thread, jshclass);
+#if ECMASCRIPT_ENABLE_IC
+        JSHClass::NotifyHclassChanged(thread, jshclass, newHclass);
+#endif
         obj->SynchronizedSetClass(thread, *newHclass);
         JSHClass::TryRestoreElementsKind(thread, newHclass, obj);
     }
