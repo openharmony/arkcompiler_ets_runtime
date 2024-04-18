d0a1d93059c2c260e87b5010967775d1832f5b54
Gymee <yumeijie@huawei.com>
2024-04-18 17:16:25 +0800

    Fix and optimize BuiltinsTypedArray::From
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/I9HR7X
    Signed-off-by: Gymee <yumeijie@huawei.com>
    Change-Id: Ic3d1d97b6a0f6a75320ab359f1ddc0ab46446c1a

--- ecmascript/builtins/builtins_typedarray.cpp
+++ ecmascript/builtins/builtins_typedarray.cpp
@@ -183,31 +183,12 @@ JSTaggedValue BuiltinsTypedArray::From(EcmaRuntimeCallInfo *argv)
     JSHandle<JSTaggedValue> source = GetCallArg(argv, 0);
     JSHandle<JSTaggedValue> iteratorSymbol = env->GetIteratorSymbol();
     JSHandle<JSTaggedValue> usingIterator = JSObject::GetMethod(thread, source, iteratorSymbol);
-    RETURN_EXCEPTION_IF_ABRUPT_COMPLETION(thread);
-    JSHandle<JSTaggedValue> arrIter = JSObject::GetMethod(thread, env->GetArrayProtoValuesFunction(), iteratorSymbol);
-    RETURN_EXCEPTION_IF_ABRUPT_COMPLETION(thread);
-    JSHandle<JSTaggedValue> typedArrIter = JSObject::GetMethod(thread, env->GetTypedArrayPrototype(), iteratorSymbol);
-    RETURN_EXCEPTION_IF_ABRUPT_COMPLETION(thread);
-    bool isArrIter = JSTaggedValue::SameValue(usingIterator, arrIter);
-    bool isTypedArrIter = JSTaggedValue::SameValue(usingIterator, typedArrIter);
-    bool isNativeFunc = true;
-    if (source->IsTypedArray() && !typedArrIter->IsUndefined()) {
-        JSHandle<JSTaggedValue> typedArrIterator = JSIterator::GetIterator(thread, source, typedArrIter);
-        RETURN_EXCEPTION_IF_ABRUPT_COMPLETION(thread);
-        JSHandle<JSTaggedValue> nextKey(thread->GlobalConstants()->GetHandledNextString());
-        JSHandle<JSTaggedValue> typedArrIterNext(JSObject::GetMethod(thread, typedArrIterator, nextKey));
-        RETURN_EXCEPTION_IF_ABRUPT_COMPLETION(thread);
-        if (typedArrIterNext->IsJSFunction()) {
-            JSTaggedValue method = JSHandle<JSFunction>::Cast(typedArrIterNext)->GetMethod();
-            Method *target = Method::Cast(method.GetTaggedObject());
-            isNativeFunc = target->IsNativeWithCallField();
-        }
-    }
     // 6. If usingIterator is not undefined, then
     //   a. Let values be ? IterableToList(source, usingIterator).
     //   b. Let len be the number of elements in values.
     //   c. Let targetObj be ? TypedArrayCreate(C, « len »).
-    if (!usingIterator->IsUndefined() && !(isArrIter || (isTypedArrIter && isNativeFunc))) {
+    if (!usingIterator->IsUndefined() &&
+        !TypedArrayHelper::IsNativeArrayIterator(thread, source, usingIterator)) {
         CVector<JSHandle<JSTaggedValue>> vec;
         JSHandle<JSTaggedValue> iterator = JSIterator::GetIterator(thread, source, usingIterator);
         RETURN_EXCEPTION_IF_ABRUPT_COMPLETION(thread);
