3ecb5d716b36b475db31767fa3ad15747eabfad4
transparent123123 <tanzirui@huawei.com>
2024-07-20 01:34:09 -0700

    IsEntry is wrong
    Issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAEDJO
    
    Signed-off-by: transparent123123 <tanzirui@huawei.com>
    Change-Id: I4662a879387ce2b5d132bd52a83d3a2a0d7ae690
    Signed-off-by: transparent123123 <tanzirui@huawei.com>

--- ecmascript/interpreter/frame_handler.h
+++ ecmascript/interpreter/frame_handler.h
@@ -62,9 +62,19 @@ public:
     {
         ASSERT(HasFrame());
         // The structure of InterpretedFrame, AsmInterpretedFrame, InterpretedEntryFrame is the same, order is pc, base.
-        FrameIterator it(sp_);
-        InterpretedFrame *state = it.GetFrame<InterpretedFrame>();
-        return state->GetPc() == nullptr;
+        FrameType type = GetFrameType();
+        if (type == FrameType::BUILTIN_ENTRY_FRAME ||
+            type == FrameType::INTERPRETER_ENTRY_FRAME ||
+            type == FrameType::ASM_INTERPRETER_ENTRY_FRAME ||
+            type == FrameType::OPTIMIZED_ENTRY_FRAME) {
+                return true;
+        } else if (type == FrameType::ASM_INTERPRETER_BRIDGE_FRAME ||
+                   type == FrameType::INTERPRETER_FRAME) {
+            FrameIterator it(sp_);
+            InterpretedFrame *state = it.GetFrame<InterpretedFrame>();
+            return state->GetPc() == nullptr;
+        }
+        return false;
     }
 
     bool IsInterpretedFrame() const
