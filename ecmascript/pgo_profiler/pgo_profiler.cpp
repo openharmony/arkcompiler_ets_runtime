01c5a23031d856afdfa335066132e3786c50484f
zhangyukun8 <zhangyukun8@huawei.com>
2024-07-25 09:12:16 +0800

    Allow to enter aot for new functions
    1. Allow to enter aot for new functions
    2. Add transitionProtoType filter
    
    Issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAFE2E?from=project-issue
    
    Signed-off-by: zhangyukun8 <zhangyukun8@huawei.com>
    Change-Id: I533784c12dcb08799f7b1d17eeba2ecddae90053

--- ecmascript/pgo_profiler/pgo_profiler.cpp
+++ ecmascript/pgo_profiler/pgo_profiler.cpp
@@ -1211,6 +1211,11 @@ void PGOProfiler::TryDumpProtoTransitionType(JSHClass *hclass)
         return;
     }
     JSTaggedValue phc1Root = JSHClass::FindProtoRootHClass(ihc1);
+    auto transitionProtoType = GetProfileType(JSHClass::Cast(phc1Root.GetTaggedObject()), true);
+    if (!transitionProtoType.IsRootType()) {
+        LOG_ECMA(DEBUG) << "Set as the prototype of a function again after transition happened for this prototype!";
+        return;
+    }
 
     auto thread = vm_->GetJSThread();
     auto *transitionTable = thread->GetCurrentEcmaContext()->GetFunctionProtoTransitionTable();
@@ -1219,6 +1224,7 @@ void PGOProfiler::TryDumpProtoTransitionType(JSHClass *hclass)
     if ((ihc0 == 0) || (baseIhc == 0)) {
         return;
     }
+
     auto ihc0Obj = JSHClass::Cast(JSTaggedValue(ihc0).GetTaggedObject());
     auto baseIhcObj = JSHClass::Cast(JSTaggedValue(baseIhc).GetTaggedObject());
     UpdateLayout(ihc0Obj);
@@ -1227,12 +1233,10 @@ void PGOProfiler::TryDumpProtoTransitionType(JSHClass *hclass)
 
     auto ihc0RootType = GetProfileType(ihc0Obj);
     ASSERT(ihc0RootType.IsRootType());
-    auto transitionProtoType = GetProfileType(JSHClass::Cast(phc1Root.GetTaggedObject()), true);
-    ASSERT(transitionProtoType.IsRootType());
     auto baseRootHClass = JSHClass::FindRootHClass(baseIhcObj);
     auto baseRootType = GetProfileType(baseRootHClass, true);
     if (!baseRootType.IsRootType()) {
-        LOG_ECMA(WARN) << "Unsupported prototypes which cannot be recorded!";
+        LOG_ECMA(DEBUG) << "Unsupported prototypes which cannot be recorded!";
         return;
     }
     auto baseType = GetProfileType(baseIhcObj);
