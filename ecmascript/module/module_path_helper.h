ec11eca36e0ec63c6b0e9df43fbc69806c5e72a7
chenlincl3 <chenlin201@huawei.com>
2024-07-28 15:00:45 +0800

    Allow ohmurl to be mixed
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAG1AT
    Signed-off-by: chenlincl3 <chenlin201@huawei.com>
    Change-Id: I45f1ed0163d39cf3064d070b7ad8e1187eca3600

--- ecmascript/module/module_path_helper.h
+++ ecmascript/module/module_path_helper.h
@@ -189,8 +189,9 @@ public:
                                                    CString &baseFileName, const CString &recordName,
                                                    CString requestName);
     static CVector<CString> SplitNormalizedRecordName(const CString &recordName);
-    static CString ConcatImportFileNormalizedOhmurlWithRecordName(const JSPandaFile *jsPandaFile,
-        const CString &recordName, CString &requestName);
+    static CString ConcatImportFileNormalizedOhmurlWithRecordName(JSThread *thread, const JSPandaFile *jsPandaFile,
+                                                                  CString &baseFileName, const CString &recordName,
+                                                                  const CString &requestName);
     static void ConcatOtherNormalizedOhmurl(EcmaVM *vm, const JSPandaFile *jsPandaFile,
                                             [[maybe_unused]] CString &baseFileName, CString &requestPath);
     static CString ConcatNormalizedOhmurlWithData(CVector<CString> &data, CString &pkgName, CString &entryPath);
