132fc63821d58d9652e3ee7ac51536ac154dad84
harryhrp <herongpeng@huawei.com>
2024-07-29 11:29:20 +0800

    Add GetAotVersion dlopen function
    add GetAotVersion dlopen function
    
    Issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAG5Y9
    
    Signed-off-by: harryhrp <herongpeng@huawei.com>
    Change-Id: Iabb348871dd99e8cdc2d39a602ca28cf911f7244

--- ecmascript/compiler/aot_file/aot_version.h
+++ ecmascript/compiler/aot_file/aot_version.h
@@ -29,6 +29,7 @@ public:
     static constexpr bool AN_STRICT_MATCH = true;
     static constexpr base::FileHeaderBase::VersionType AI_VERSION = {4, 0, 0, 4};
     static constexpr bool AI_STRICT_MATCH = true;
+    constexpr static const int VERSION_MULTIPLE_SIZE = 2;
     static PUBLIC_API std::string GetAOTVersion()
     {
         return base::FileHeaderBase::ConvToStr(AN_VERSION);
@@ -43,5 +44,7 @@ public:
         return true;
     }
 };
+
+extern "C" int32_t GetAOTVersion(std::string &aotVersion);
 }  // namespace panda::ecmascript
 #endif  // ECMASCRIPT_AOT_VERSION_H
