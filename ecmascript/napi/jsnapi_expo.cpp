a1e381391fbdee54c5a98cc7fa5b6eaecf98f3fe
xiongluo <xiongluo@huawei.com>
2024-08-07 15:49:22 +0800

    Fix maybe unused params in jsnapi expo
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAIHQ6?from=project-issue
    
    Signed-off-by: xiongluo <xiongluo@huawei.com>
    Change-Id: I35b4537a8a12edf40f9f8079e44a93c4c357edcb

--- ecmascript/napi/jsnapi_expo.cpp
+++ ecmascript/napi/jsnapi_expo.cpp
@@ -4756,7 +4756,7 @@ bool JSNApi::StopDebugger([[maybe_unused]] int tid)
 #endif // ECMASCRIPT_SUPPORT_DEBUGGER
 }
 
-bool JSNApi::IsMixedDebugEnabled(const EcmaVM *vm)
+bool JSNApi::IsMixedDebugEnabled([[maybe_unused]] const EcmaVM *vm)
 {
 #if defined(ECMASCRIPT_SUPPORT_DEBUGGER)
     return vm->GetJsDebuggerManager()->IsMixedDebugEnabled();
@@ -4765,7 +4765,7 @@ bool JSNApi::IsMixedDebugEnabled(const EcmaVM *vm)
 #endif
 }
 
-void JSNApi::NotifyNativeCalling(const EcmaVM *vm, [[maybe_unused]] const void *nativeAddress)
+void JSNApi::NotifyNativeCalling([[maybe_unused]] const EcmaVM *vm, [[maybe_unused]] const void *nativeAddress)
 {
 #if defined(ECMASCRIPT_SUPPORT_DEBUGGER)
     CROSS_THREAD_AND_EXCEPTION_CHECK(vm);
@@ -4775,7 +4775,7 @@ void JSNApi::NotifyNativeCalling(const EcmaVM *vm, [[maybe_unused]] const void *
 #endif
 }
 
-void JSNApi::NotifyNativeReturn(const EcmaVM *vm,  [[maybe_unused]] const void *nativeAddress)
+void JSNApi::NotifyNativeReturn([[maybe_unused]] const EcmaVM *vm,  [[maybe_unused]] const void *nativeAddress)
 {
 #if defined(ECMASCRIPT_SUPPORT_DEBUGGER)
     CROSS_THREAD_AND_EXCEPTION_CHECK(vm);
@@ -4785,7 +4785,7 @@ void JSNApi::NotifyNativeReturn(const EcmaVM *vm,  [[maybe_unused]] const void *
 #endif
 }
 
-void JSNApi::NotifyLoadModule(const EcmaVM *vm)
+void JSNApi::NotifyLoadModule([[maybe_unused]] const EcmaVM *vm)
 {
 #if defined(ECMASCRIPT_SUPPORT_DEBUGGER)
     CROSS_THREAD_AND_EXCEPTION_CHECK(vm);
