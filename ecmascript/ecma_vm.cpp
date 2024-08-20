7944b97a9e8a405ba6810305938a30a584a06883
jinjiawei <jinjiawei2@huawei.com>
2024-08-20 10:55:09 +0800

    cherry pick 4b3f875 from https://gitee.com/wwzm-jjw/arkcompiler_ets_runtime/pulls/8760 Fix loop idle sharedgc deadlock
    Issue:    https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAL5EJ
    Signed-off-by: jinjiawei <jinjiawei2@huawei.com>

--- ecmascript/ecma_vm.cpp
+++ ecmascript/ecma_vm.cpp
@@ -793,8 +793,9 @@ void EcmaVM::TriggerConcurrentCallback(JSTaggedValue result, JSTaggedValue hint)
     void *taskInfo = reinterpret_cast<void*>(thread_->GetTaskInfo());
     // clear the taskInfo when return, which can prevent the callback to get it
     thread_->SetTaskInfo(reinterpret_cast<uintptr_t>(nullptr));
-    concurrentCallback_(JSNApiHelper::ToLocal<JSValueRef>(JSHandle<JSTaggedValue>(thread_, result)), success,
-                        taskInfo, concurrentData_);
+    auto localResultRef = JSNApiHelper::ToLocal<JSValueRef>(JSHandle<JSTaggedValue>(thread_, result));
+    ThreadNativeScope nativeScope(thread_);
+    concurrentCallback_(localResultRef, success, taskInfo, concurrentData_);
 }
 
 void EcmaVM::DumpCallTimeInfo()
