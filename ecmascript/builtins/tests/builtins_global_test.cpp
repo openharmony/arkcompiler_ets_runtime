97e9b9072f434c40da6453eb8db58a2dc00f8954
lhc <liuhongchen5@huawei.com>
2024-08-28 23:24:31 +0800

    Add IsSendable ut test cases
    Add IsSendable ut test cases
    
    issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAN4OO?from=project-issue
    
    Signed-off-by: lhc <liuhongchen5@huawei.com>
    Change-Id: Id9d9c8f302989081e700736c0045be709bc0bd60

--- ecmascript/builtins/tests/builtins_global_test.cpp
+++ ecmascript/builtins/tests/builtins_global_test.cpp
@@ -22,6 +22,8 @@
 #include "ecmascript/js_tagged_value-inl.h"
 #include "ecmascript/js_thread.h"
 #include "ecmascript/object_factory.h"
+#include "ecmascript/js_array.h"
+#include "ecmascript/shared_objects/js_shared_array.h"
 #include "ecmascript/tests/test_helper.h"
 
 using namespace panda::ecmascript;
@@ -184,4 +186,178 @@ HWTEST_F_L0(BuiltinsGlobalTest, Unescape)
     JSHandle<EcmaString> ecmaStrHandle3(thread, result3);
     EXPECT_STREQ("Hello%2520World%2521", EcmaStringAccessor(ecmaStrHandle3).ToCString().c_str());
 }
+
+HWTEST_F_L0(BuiltinsGlobalTest, isSendable_0)
+{
+    ObjectFactory *factory = thread->GetEcmaVM()->GetFactory();
+    JSHandle<EcmaString> str1 = factory->NewFromASCII("?!=()#%&");
+    auto ecmaRuntimeCallInfo1 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo1->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo1->SetThis(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo1->SetCallArg(0, str1.GetTaggedValue());
+
+    [[maybe_unused]] auto prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo1);
+    JSTaggedValue result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo1);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::True(), result);
+
+    auto ecmaRuntimeCallInfo2 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo2->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo2->SetThis(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo2->SetCallArg(0, JSTaggedValue(1));
+
+    prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo2);
+    result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo2);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::True(), result);
+
+    auto ecmaRuntimeCallInfo3 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo3->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo3->SetThis(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo3->SetCallArg(0, JSTaggedValue(1.5));
+
+    prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo3);
+    result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo3);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::True(), result);
+}
+
+HWTEST_F_L0(BuiltinsGlobalTest, isSendable_1)
+{
+    ObjectFactory *factory = thread->GetEcmaVM()->GetFactory();
+
+    auto ecmaRuntimeCallInfo5 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo5->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo5->SetThis(JSTaggedValue::Undefined());
+    JSFunction *jsFunc = thread->GetEcmaVM()->GetGlobalEnv()->GetObjectFunction().GetObject<JSFunction>();
+    JSHandle<JSTaggedValue> jsFunc1(thread, jsFunc);
+    JSHandle<JSObject> jsObj = factory->NewJSObjectByConstructor(JSHandle<JSFunction>(jsFunc1), jsFunc1);
+    ecmaRuntimeCallInfo5->SetCallArg(0, jsObj.GetTaggedValue());
+
+    [[maybe_unused]] auto prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo5);
+    auto result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo5);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::False(), result);
+
+    auto ecmaRuntimeCallInfo6 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo6->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo6->SetThis(JSTaggedValue::Undefined());
+    JSHandle<JSObject>  sharedArrayFunc(thread->GetEcmaVM()->GetGlobalEnv()->GetSharedArrayFunction());
+    ecmaRuntimeCallInfo6->SetCallArg(0, sharedArrayFunc.GetTaggedValue());
+
+    prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo6);
+    result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo6);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::True(), result);
+
+    auto ecmaRuntimeCallInfo8 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo8->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo8->SetThis(JSTaggedValue::Undefined());
+    JSArray *arr = JSArray::Cast(JSArray::ArrayCreate(thread, JSTaggedNumber(0)).GetTaggedValue().GetTaggedObject());
+    EXPECT_TRUE(arr != nullptr);
+    JSHandle<JSObject> arrObj(thread, arr);
+    ecmaRuntimeCallInfo8->SetCallArg(0, arrObj.GetTaggedValue());
+
+    prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo8);
+    result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo8);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::False(), result);
+}
+
+HWTEST_F_L0(BuiltinsGlobalTest, isSendable_2)
+{
+    auto ecmaRuntimeCallInfo4 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo4->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo4->SetThis(JSTaggedValue::Undefined());
+    JSFunction *jsFunc = thread->GetEcmaVM()->GetGlobalEnv()->GetObjectFunction().GetObject<JSFunction>();
+    JSHandle<JSTaggedValue> jsFunc1(thread, jsFunc);
+    ecmaRuntimeCallInfo4->SetCallArg(0, jsFunc1.GetTaggedValue());
+
+    [[maybe_unused]] auto prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo4);
+    auto result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo4);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::False(), result);
+
+    auto ecmaRuntimeCallInfo7 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo7->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo7->SetThis(JSTaggedValue::Undefined());
+    JSHandle<JSObject>  sharedArrayFunc(thread->GetEcmaVM()->GetGlobalEnv()->GetSharedArrayFunction());
+    JSSharedArray *arr = JSSharedArray::Cast(JSSharedArray::ArrayCreate(thread, JSTaggedNumber(0))->GetTaggedObject());
+    EXPECT_TRUE(arr != nullptr);
+    JSHandle<JSObject> sharedObj(thread, arr);
+    ecmaRuntimeCallInfo7->SetCallArg(0, sharedObj.GetTaggedValue());
+
+    prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo7);
+    result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo7);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::True(), result);
+
+    auto ecmaRuntimeCallInfo9 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 8);
+    ecmaRuntimeCallInfo9->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo9->SetThis(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo9->SetCallArg(0, sharedObj.GetTaggedValue());
+    ecmaRuntimeCallInfo9->SetCallArg(1, sharedObj.GetTaggedValue());
+
+    prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo9);
+    result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo9);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::False(), result);
+}
+
+HWTEST_F_L0(BuiltinsGlobalTest, isSendable_3)
+{
+    auto ecmaRuntimeCallInfo4 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo4->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo4->SetThis(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo4->SetCallArg(0, JSTaggedValue::Undefined());
+
+    [[maybe_unused]] auto prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo4);
+    auto result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo4);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::True(), result);
+
+    auto ecmaRuntimeCallInfo5 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo5->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo5->SetThis(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo5->SetCallArg(0, JSTaggedValue::True());
+
+    prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo5);
+    result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo5);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::True(), result);
+
+    auto ecmaRuntimeCallInfo6 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 8);
+    ecmaRuntimeCallInfo6->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo6->SetThis(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo6->SetCallArg(0, JSTaggedValue::Exception());
+
+    prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo6);
+    result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo6);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::False(), result);
+
+    auto ecmaRuntimeCallInfo9 =
+        TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+    ecmaRuntimeCallInfo9->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo9->SetThis(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo9->SetCallArg(0, JSTaggedValue::Null());
+
+    prev1 = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo9);
+    result = BuiltinsGlobal::IsSendable(ecmaRuntimeCallInfo9);
+    TestHelper::TearDownFrame(thread, prev1);
+    EXPECT_EQ(JSTaggedValue::True(), result);
+}
+
 }  // namespace panda::test
