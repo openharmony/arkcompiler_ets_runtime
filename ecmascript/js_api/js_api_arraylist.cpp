39d3b4c14c1c2e2e6fd72bc9dcab67c957d734e3
dingwen <dingwen6@huawei.com>
2024-08-29 11:41:20 +0800

    Fix JSApiArrayList clear overflow （cherry picked commit from <gitee.com//openharmony/arkcompiler_ets_runtime/commit/9f2b277c3790f68e389ee8f95c9158db82f2479e>
    Signed-off-by: dingwen <dingwen6@huawei.com>
    Change-Id: If6c1ad0640a491301bd7c791fed035cacb1463ae

--- ecmascript/js_api/js_api_arraylist.cpp
+++ ecmascript/js_api/js_api_arraylist.cpp
@@ -66,7 +66,7 @@ void JSAPIArrayList::Clear(JSThread *thread, const JSHandle<JSAPIArrayList> &arr
         int length = arrayList->GetLength().GetInt();
         JSHandle<TaggedArray> elements(thread, arrayList->GetElements());
         ASSERT(!elements->IsDictionaryMode());
-        for (int i = 0; i <= length; ++i) {
+        for (int i = 0; i < length; ++i) {
             elements->Set(thread, i, JSTaggedValue::Hole());
         }
         arrayList->SetLength(thread, JSTaggedValue(0));
