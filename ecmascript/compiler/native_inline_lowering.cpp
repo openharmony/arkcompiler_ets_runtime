98ad86de0253bc84d0f2acf5c5376a27b335159d
luobinghao <luobinghao@huawei.com>
2024-09-08 14:37:37 +0800

    fix different between yellow and blue
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAPOSC?from=project-issue
    
    Signed-off-by: luobinghao <luobinghao@huawei.com>
    Change-Id: If61be3cdcc706e1cdf81e51891a8386e4f50f60a

--- ecmascript/compiler/native_inline_lowering.cpp
+++ ecmascript/compiler/native_inline_lowering.cpp
@@ -1511,11 +1511,11 @@ void NativeInlineLowering::TryInlineArrayIterator(GateRef gate, BuiltinsStubCSig
     if (!Uncheck()) {
         builder_.CallTargetCheck(gate, tacc.GetFunc(), builder_.IntPtr(static_cast<int64_t>(id)), {tacc.GetThisObj()});
     }
+    GateRef thisObj = acc_.GetValueIn(gate, 0);
+    builder_.EcmaObjectCheck(thisObj);
     if (EnableTrace()) {
         AddTraceLogs(gate, id);
     }
-    GateRef thisObj = acc_.GetValueIn(gate, 0);
-    builder_.EcmaObjectCheck(thisObj);
     GateRef CallIDRef = builder_.Int32(static_cast<int32_t>(id));
     GateRef ret = builder_.ArrayIteratorBuiltin(thisObj, CallIDRef);
     acc_.ReplaceHirAndDeleteIfException(gate, builder_.GetStateDepend(), ret);
