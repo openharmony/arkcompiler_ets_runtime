10fb3a645567c1ab005102d747092ec3c12833df
yangxiaoshuai2022 <yangxiaoshuai@huawei.com>
2024-09-11 09:30:46 +0800

    rectification of inconsistent code
    Issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAQERW
    
    Signed-off-by: yangxiaoshuai2022 <yangxiaoshuai@huawei.com>

--- ecmascript/js_thread.h
+++ ecmascript/js_thread.h
@@ -1366,6 +1366,14 @@ public:
         finalizeTaskCallback_ = callback;
     }
 
+    uint64_t GetJobId()
+    {
+        if (jobId_ == UINT64_MAX) {
+            jobId_ = 0;
+        }
+        return ++jobId_;
+    }
+
     void SetAsyncCleanTaskCallback(const NativePointerTaskCallback &callback)
     {
         asyncCleanTaskCb_ = callback;
@@ -1417,14 +1425,6 @@ public:
     bool IsMachineCodeLowMemory()
     {
         return machineCodeLowMemory_;
-    }
-
-    uint64_t GetJobId()
-    {
-        if (jobId_ == UINT64_MAX) {
-            jobId_ = 0;
-        }
-        return ++jobId_;
     }
 
     void *GetEnv() const
@@ -1599,12 +1599,12 @@ private:
     ConditionVariable suspendCondVar_;
     SuspendBarrier *suspendBarrier_ {nullptr};
 
+    uint64_t jobId_ {0};
+
     ThreadType threadType_ {ThreadType::JS_THREAD};
     RecursiveMutex jitMutex_;
     bool machineCodeLowMemory_ {false};
     RecursiveMutex profileTypeAccessorLockMutex_;
-
-    uint64_t jobId_ {0};
 
 #ifndef NDEBUG
     MutatorLock::MutatorLockState mutatorLockState_ = MutatorLock::MutatorLockState::UNLOCKED;
