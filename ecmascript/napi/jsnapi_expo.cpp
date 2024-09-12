0e7c2a99184d78d44a6f40d533c4f63c5177f3e5
milkpotatoes <chenshulin5@huawei.com>
2024-09-12 10:51:56 +0800

    Add test cases for JSNApi
    Add unittest cases for JSNApi
    rename file name of JSNApi docs
    
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAQUSP
    
    Signed-off-by: milkpotatoes <chenshulin5@huawei.com>
    Change-Id: I24e74696f677a8e149fa5bcd26f0bfd6622dba96

--- ecmascript/napi/jsnapi_expo.cpp
+++ ecmascript/napi/jsnapi_expo.cpp
@@ -5391,6 +5391,7 @@ void JSNApi::DisposeGlobalHandleAddr(const EcmaVM *vm, uintptr_t addr)
     if (addr == 0 || !reinterpret_cast<ecmascript::Node *>(addr)->IsUsing()) {
         return;
     }
+
     ecmascript::ThreadManagedScope scope(vm->GetJSThread());
     CROSS_THREAD_CHECK(vm);
     thread->DisposeGlobalHandle(addr);
@@ -5775,7 +5776,6 @@ Local<ObjectRef> JSNApi::GetModuleNameSpaceFromFile(EcmaVM *vm, const std::strin
         // need get moduleName from stack
         std::pair<std::string, std::string> moduleInfo = vm->GetCurrentModuleInfo(false);
         if (thread->HasPendingException()) {
-            ecmascript::JsStackInfo::BuildCrashInfo(thread);
             thread->GetCurrentEcmaContext()->HandleUncaughtException();
             return JSValueRef::Undefined(vm);
         }
