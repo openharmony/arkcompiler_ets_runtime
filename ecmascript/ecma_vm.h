809829069666a1a6118db65308f812d1ac202849
chentianyu <chentianyu31@huawei.com>
2024-09-18 17:57:24 +0800

    Fix GC callback may not execute
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IARXW3
    
    Change-Id: I32f5c32c1535d09d0ae0ff7f3d45dfe37b03ba8e
    Signed-off-by: chentianyu <chentianyu31@huawei.com>

--- ecmascript/ecma_vm.h
+++ ecmascript/ecma_vm.h
@@ -718,9 +718,9 @@ public:
         return concurrentNativeCallbacks_;
     }
 
-    std::vector<NativePointerCallbackData> &GetAsyncNativePointerCallbacks()
+    AsyncNativeCallbacksPack &GetAsyncNativePointerCallbacksPack()
     {
-        return asyncNativeCallbacks_;
+        return asyncNativeCallbacksPack_;
     }
 
     void SetIsJitCompileVM(bool isJitCompileVM)
@@ -902,7 +902,7 @@ private:
     CList<JSNativePointer *> nativePointerList_;
     CList<JSNativePointer *> concurrentNativePointerList_;
     std::vector<NativePointerCallbackData> concurrentNativeCallbacks_ {};
-    std::vector<NativePointerCallbackData> asyncNativeCallbacks_ {};
+    AsyncNativeCallbacksPack asyncNativeCallbacksPack_;
     CList<JSNativePointer *> sharedNativePointerList_;
     std::vector<std::pair<NativePointerCallback, std::pair<void *, void *>>> sharedNativePointerCallbacks_ {};
     // VM execution states.
