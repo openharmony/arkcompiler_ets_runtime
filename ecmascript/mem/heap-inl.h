ac19a0b88df518a62f3ff3c3a23ea95415198e72
zhouwenxuan <zhouwenxuan1@huawei.com>
2024-10-11 10:21:48 +0800

    修复object_factory.cpp和heap-inl.h文件中存在未判断指针有效性问题
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAW7GD
    
    Signed-off-by: zhouwenxuan <zhouwenxuan1@huawei.com>
    Change-Id: I3e464578990a00f5e9b4d191cdc0b9b8745b1835

--- ecmascript/mem/heap-inl.h
+++ ecmascript/mem/heap-inl.h
@@ -239,6 +239,7 @@ TaggedObject *Heap::AllocateInGeneralNewSpace(size_t size)
 TaggedObject *Heap::AllocateYoungOrHugeObject(JSHClass *hclass, size_t size)
 {
     auto object = AllocateYoungOrHugeObject(size);
+    ASSERT(object != nullptr);
     object->SetClass(thread_, hclass);
 #if defined(ECMASCRIPT_SUPPORT_HEAPPROFILER)
     OnAllocateEvent(GetEcmaVM(), object, size);
@@ -249,6 +250,7 @@ TaggedObject *Heap::AllocateYoungOrHugeObject(JSHClass *hclass, size_t size)
 void BaseHeap::SetHClassAndDoAllocateEvent(JSThread *thread, TaggedObject *object, JSHClass *hclass,
                                            [[maybe_unused]] size_t size)
 {
+    ASSERT(object != nullptr);
     object->SetClass(thread, hclass);
 #if defined(ECMASCRIPT_SUPPORT_HEAPPROFILER)
     OnAllocateEvent(thread->GetEcmaVM(), object, size);
