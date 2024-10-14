c8a56ce04c13bd8b4254990e0123d0af63c68c1b
xwcai98 <caixinwei5@huawei.com>
2024-10-14 21:37:58 +0800

    CP 'Sweep stringtable in parallel' to 5.0.1
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAX2VC
    
    Signed-off-by: xwcai98 <caixinwei5@huawei.com>
    Change-Id: I626c09e00d3f66c497853f0d3b42cbf99e791a7a

--- ecmascript/mem/shared_heap/shared_gc.cpp
+++ ecmascript/mem/shared_heap/shared_gc.cpp
@@ -108,7 +108,8 @@ void SharedGC::Sweep()
         }
         return nullptr;
     };
-    Runtime::GetInstance()->GetEcmaStringTable()->SweepWeakReference(gcUpdateWeak);
+    auto stringTableCleaner = Runtime::GetInstance()->GetEcmaStringTable()->GetCleaner();
+    stringTableCleaner->PostSweepWeakRefTask(gcUpdateWeak);
     Runtime::GetInstance()->ProcessNativeDeleteInSharedGC(gcUpdateWeak);
 
     Runtime::GetInstance()->GCIterateThreadList([&](JSThread *thread) {
@@ -118,6 +119,7 @@ void SharedGC::Sweep()
         const_cast<Heap*>(thread->GetEcmaVM()->GetHeap())->ResetTlab();
     });
 
+    stringTableCleaner->JoinAndWaitSweepWeakRefTask(gcUpdateWeak);
     sHeap_->GetSweeper()->Sweep();
     sHeap_->GetSweeper()->PostTask();
 }
