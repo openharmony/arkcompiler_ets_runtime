2105506f277b2f0af99548e448a89e4e20b71d7e
shilei91 <shilei91@huawei.com>
2024-10-15 19:33:03 +0800

    cherry pick 778fd1d from https://gitee.com/shilei91/ark_js_runtime/pulls/9783 fix problem of RemoveNode when remove root of RBTree
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IAXC45
    
    Signed-off-by: shilei91 <shilei91@huawei.com>
    Change-Id: I248e48b980f2de1a2384d96c8718a592c51d29db

--- ecmascript/tagged_hash_array.cpp
+++ ecmascript/tagged_hash_array.cpp
@@ -250,6 +250,10 @@ JSTaggedValue TaggedHashArray::RemoveNode(JSThread *thread, int hash, JSTaggedVa
     } else if (node.IsRBTreeNode()) {
         JSTaggedValue oldValue = JSTaggedValue::Hole();
         JSTaggedValue rootTreeNodeVa = RBTreeNode::Delete(thread, node, hash, key, oldValue);
+        if (rootTreeNodeVa.IsHole()) {
+            Set(thread, index, JSTaggedValue::Hole());
+            return oldValue;
+        }
         //set root node as black
         RBTreeNode *root = RBTreeNode::Cast(rootTreeNodeVa.GetTaggedObject());
         if (root->GetIsRed().ToBoolean()) {
