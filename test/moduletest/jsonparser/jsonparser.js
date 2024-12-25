b802f94e4f065fde5425c6bb7418c1562c9d1697
leiliufc <leiliufc@isoftstone.com>
2024-12-25 15:49:47 +0800

    将 moduletest 下的 print 修改为 assert
    Signed-off-by: leiliufc <leiliufc@isoftstone.com>

--- test/moduletest/jsonparser/jsonparser.js
+++ test/moduletest/jsonparser/jsonparser.js
@@ -21,11 +21,11 @@
  */
 
 let json = JSON.parse("[ 1, 2, 3]");
-print(json);
+assert_equal(json,[1,2,3]);
 let json2 = JSON.parse("[       1       ]");
-print(json2);
+assert_equal(json2,[1]);
 let json3 = JSON.parse("[              ]");
-print(json3);
+assert_equal(json3,[]);
 let data = {
     "11111111" : "https://www.a.com",
     "22222222" : "https://www.b.com",
@@ -33,63 +33,65 @@ let data = {
 }
 let strData = JSON.stringify(data);
 let res = JSON.parse(strData);
-print(res["11111111"]);
-print(res["22222222"]);
-print(res["00000000"]);
+assert_equal(res["11111111"],'https://www.a.com');
+assert_equal(res["22222222"],'https://www.b.com');
+assert_equal(res["00000000"],'https://www.c.com');
 
 var a = `{"code": 0, "msg": "ok"}`
 function reviver(k, v) { return v; }
 var o = JSON.parse(a, reviver);
-print(o);
+assert_equal(o.toString(),'[object Object]');
 
 let strData2 = "1.7976971348623157e+308";
 let res2 = JSON.parse(strData2);
-print(res2);
+assert_equal(res2,Infinity);
 
 let strData3 = "-1.7976971348623157e+308";
 let res3 = JSON.parse(strData3);
-print(res3);
+assert_equal(res3,-Infinity);
 
 let strData4 = "123";
 let res4 = JSON.parse(strData4);
-print(res4);
+assert_equal(res4,123);
 
 try {
-    print(JSON.parse(`{"object": 42, "test":{}`))
+    JSON.parse(`{"object": 42, "test":{}`)
 } catch (error) {
-    print(error.name)
+    assert_equal(error.name,'SyntaxError')
+    
 }
 
 let strData5 = "\"\\uDC00\"";
 let res5 = JSON.parse(strData5);
-print(res5.codePointAt(0))
+assert_equal(res5.codePointAt(0),56320)
 
 let strData6 = '{"a": "{\\"width\\": 18}"}'
-print(JSON.stringify(JSON.parse(strData6)))
+assert_equal(JSON.stringify(JSON.parse(strData6)),'{"a":"{\\"width\\": 18}"}')
 
 let strData7 = '{"a": "{\\"name\\": \\"张三\\"}"}'
-print(JSON.stringify(JSON.parse(strData7)))
+assert_equal(JSON.stringify(JSON.parse(strData7)),'{"a":"{\\"name\\": \\"张三\\"}"}')
 
 let strData8 = '{"1\\u0000":"name"}'
-print(JSON.stringify(JSON.parse(strData8)))
+assert_equal(JSON.stringify(JSON.parse(strData8)),'{"1\\u0000":"name"}')
 
-print(JSON.parse('123.456e-789'));
-print(1 / JSON.parse('-0'));
+assert_equal(JSON.parse('123.456e-789'),0);
+assert_equal(1 / JSON.parse('-0'),-Infinity);
 
-print("JSON.parse with backslash")
-print(JSON.parse('"\\"\\""'));  // utf8 -> utf8
-print(JSON.parse('"\\u0055\\u0066"'));  // utf8 -> utf8
-print(JSON.parse('"\\u5555\\u6666"'));  // utf8 -> utf16
-print(JSON.parse('["\\"\\"","中文"]')[0]);  // utf16 -> utf8
-print(JSON.parse('["\\u0055\\u0066","中文"]')[0]);  // utf16 -> utf8
-print(JSON.parse('["\\u5555\\u6666","中文"]')[0]);  // utf16 -> utf16
+var string = "JSON.parse with backslash";
+assert_equal(string,"JSON.parse with backslash");
+assert_equal(JSON.parse('"\\"\\""'),'""');  // utf8 -> utf8
+assert_equal(JSON.parse('"\\u0055\\u0066"'),'Uf');  // utf8 -> utf8
+assert_equal(JSON.parse('"\\u5555\\u6666"'),'啕晦');  // utf8 -> utf16
+assert_equal(JSON.parse('["\\"\\"","中文"]')[0],'""');  // utf16 -> utf8
+assert_equal(JSON.parse('["\\u0055\\u0066","中文"]')[0],'Uf');  // utf16 -> utf8
+assert_equal(JSON.parse('["\\u5555\\u6666","中文"]')[0],'啕晦');  // utf16 -> utf16
 
 const strData9 = `{"k1":"hello","k2":3}`;
 const strErr = strData9.substring(0, strData9.length - 2);
 try {
     JSON.parse(strErr);
 } catch (err) {
-    print(err.name)
+    assert_equal(err.name,'SyntaxError');
 }
 
 const strData10 = `{"k1":"hello","k2":                    3}`;
@@ -97,7 +99,7 @@ const strErr2 = strData10.substring(0, strData10.length - 2);
 try {
     JSON.parse(strErr2);
 } catch (err) {
-    print(err.name)
+    assert_equal(err.name,'SyntaxError');
 }
 
 const strData11 = `{"k1":"hello","k2":311111}`;
@@ -105,5 +107,7 @@ const strErr3 = strData11.substring(0, strData11.length - 2);
 try {
     JSON.parse(strErr3);
 } catch (err) {
-    print(err.name)
+    assert_equal(err.name,'SyntaxError');
 }
+
+test_end();
