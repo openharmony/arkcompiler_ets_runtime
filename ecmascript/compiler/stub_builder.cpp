a94eb7a6642931c6499bd9e40554f59ae2a93dd2
wuxiesaber <wuxie@huawei.com>
2024-12-26 19:12:51 +0800

    add taggedOject check in GetIterator
    https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IBDZ1R
    
    Signed-off-by: wuxiesaber <wuxie@huawei.com>
    Change-Id: Ic82f8fb67a858e79a985ef687b5d0b8476f006dd

--- ecmascript/compiler/stub_builder.cpp
+++ ecmascript/compiler/stub_builder.cpp
@@ -8736,7 +8736,7 @@ GateRef StubBuilder::GetIterator(GateRef glue, GateRef obj, ProfileOperation cal
     Label exit(env);
     env->SubCfgEntry(&entryPass);
     DEFVARIABLE(result, VariableType::JS_ANY(), Exception());
-    DEFVARIABLE(taggedId, VariableType::INT32(), Int32(0));
+    DEFVARIABLE(taggedId, VariableType::INT32(), Int32(GET_MESSAGE_STRING_ID(ObjIsNotCallable)));
 
     Label isPendingException(env);
     Label noPendingException(env);
@@ -8813,14 +8813,18 @@ GateRef StubBuilder::GetIterator(GateRef glue, GateRef obj, ProfileOperation cal
         if (env->IsBaselineBuiltin()) {
             callBuilder.JSCallDispatchForBaseline(&callExit);
             Bind(&callExit);
+            Jump(&exit);
         } else {
             result = callBuilder.JSCallDispatch();
+            Label modifyErrorInfo(env);
+            BRANCH(TaggedIsHeapObject(*result), &exit, &modifyErrorInfo);
+            Bind(&modifyErrorInfo);
+            taggedId = Int32(GET_MESSAGE_STRING_ID(IterNotObject));
+            Jump(&throwError);
         }
-        Jump(&exit);
     }
     Bind(&throwError);
     {
-        taggedId = Int32(GET_MESSAGE_STRING_ID(ObjIsNotCallable));
         CallRuntime(glue, RTSTUB_ID(ThrowTypeError), { IntToTaggedInt(*taggedId) });
         result = Exception();
         Jump(&exit);
@@ -8838,7 +8842,7 @@ GateRef StubBuilder::GetIterator(GateRef glue, GateRef obj, ProfileOperation cal
     Label exit(env);
     env->SubCfgEntry(&entryPass);
     DEFVARIABLE(result, VariableType::JS_ANY(), Exception());
-    DEFVARIABLE(taggedId, VariableType::INT32(), Int32(0));
+    DEFVARIABLE(taggedId, VariableType::INT32(), Int32(GET_MESSAGE_STRING_ID(ObjIsNotCallable)));
 
     Label isPendingException(env);
     Label noPendingException(env);
@@ -8869,14 +8873,18 @@ GateRef StubBuilder::GetIterator(GateRef glue, GateRef obj, ProfileOperation cal
         if (env->IsBaselineBuiltin()) {
             callBuilder.JSCallDispatchForBaseline(&callExit);
             Bind(&callExit);
+            Jump(&exit);
         } else {
             result = callBuilder.JSCallDispatch();
+            Label modifyErrorInfo(env);
+            BRANCH(TaggedIsHeapObject(*result), &exit, &modifyErrorInfo);
+            Bind(&modifyErrorInfo);
+            taggedId = Int32(GET_MESSAGE_STRING_ID(IterNotObject));
+            Jump(&throwError);
         }
-        Jump(&exit);
     }
     Bind(&throwError);
     {
-        taggedId = Int32(GET_MESSAGE_STRING_ID(ObjIsNotCallable));
         CallRuntime(glue, RTSTUB_ID(ThrowTypeError), { IntToTaggedInt(*taggedId) });
         result = Exception();
         Jump(&exit);
