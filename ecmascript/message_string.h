3387e19198eaf9fa78e77837c27b5fe18a090b48
wuxiesaber <wuxie@huawei.com>
2024-12-26 19:12:51 +0800

    add taggedOject check in GetIterator
    https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IBDZ1R
    
    Signed-off-by: wuxiesaber <wuxie@huawei.com>
    Change-Id: Ic82f8fb67a858e79a985ef687b5d0b8476f006dd

--- ecmascript/message_string.h
+++ ecmascript/message_string.h
@@ -66,7 +66,8 @@ namespace panda::ecmascript {
     V(DefineFieldField, "DefineField: obj is not Object")                                    \
     V(IsNotPropertyKey, "key is not a property key")                                         \
     V(CreateDataPropertyFailed, "failed to create data property")                            \
-    V(ValueIsNonSObject, "value is not a shared object")
+    V(ValueIsNonSObject, "value is not a shared object")                                     \
+    V(IterNotObject, "JSIterator::GetIterator: iter is not object")
 
 #define DEBUG_CHECK_MESSAGE_STRING_LIST(V)                                                   \
     V(IsCallable)                                                                            \
