9219c2e9b5e4d9c71ec7f5048b75ee4ad13ac091
rentangyu <rentangyu@huawei.com>
2025-01-14 11:46:21 +0800

    asan hangs on the backtrace
    Issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IBHNS3
    
    Signed-off-by: rentangyu <rentangyu@huawei.com>

--- ecmascript/dfx/stackinfo/js_stackinfo.cpp
+++ ecmascript/dfx/stackinfo/js_stackinfo.cpp
@@ -224,7 +224,7 @@ std::string JsStackInfo::BuildJsStackTrace(JSThread *thread, bool needNative, co
     if (data.empty()) {
 #if defined(ENABLE_EXCEPTION_BACKTRACE)
         std::ostringstream stack;
-        Backtrace(stack);
+        Backtrace(stack, true);
         data = stack.str();
 #endif
     }
