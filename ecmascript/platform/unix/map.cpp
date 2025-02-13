4ba56649574d245e66415c5629a80f5ffcdaf85d
Vinogradov Victor <vinogradov.victor@huawei-partners.com>
2025-02-13 13:26:50 +0300

    Fixed invalid argument
    Change-Id: I2db677fb03fae99cf60360454fa9722d1eb8ef45
    Signed-off-by: Vinogradov Victor <vinogradov.victor@huawei-partners.com>

--- ecmascript/platform/unix/map.cpp
+++ ecmascript/platform/unix/map.cpp
@@ -89,7 +89,7 @@ void PageTag(void *mem, size_t size, PageTagType type, [[maybe_unused]] const st
 
 void PageClearTag(void *mem, size_t size)
 {
-    PrctlSetVMA(mem, size, nullptr);
+    PrctlSetVMA(mem, size, "");
 }
 
 bool PageProtect(void *mem, size_t size, int prot)
