2f61d76c4f4c5c3d8c7bac53ad8b960a6c3b7ff1
Vinogradov Victor <vinogradov.victor@huawei-partners.com>
2025-02-20 16:28:25 +0300

    Moving inline function inside class declaration
    Signed-off-by: Vinogradov Victor <vinogradov.victor@huawei-partners.com>
    Change-Id: Icf98d6f4f3cafdc276c3995a0fccf2262b6b364b

--- ecmascript/tagged_array-inl.h
+++ ecmascript/tagged_array-inl.h
@@ -44,26 +44,6 @@ inline void TaggedArray::Set(const JSThread *thread, uint32_t idx, const T &valu
     }
 }
 
-#ifndef ECMASCRIPT_TAGGED_ARRAY_CPP
-// `Get` is inlined when possible, including the case when it's used outside libark_jsruntime.
-// For other cases `Get` is defined with external linkage in tagged_array.cpp
-    #define MAYBE_INLINE inline
-#else
-    #define MAYBE_INLINE
-#endif // ECMASCRIPT_TAGGED_ARRAY_CPP
-
-MAYBE_INLINE JSTaggedValue TaggedArray::Get(uint32_t idx) const
-{
-    ASSERT(idx < GetLength());
-    // Note: Here we can't statically decide the element type is a primitive or heap object, especially for
-    //       dynamically-typed languages like JavaScript. So we simply skip the read-barrier.
-    size_t offset = JSTaggedValue::TaggedTypeSize() * idx;
-    // NOLINTNEXTLINE(readability-braces-around-statements, bugprone-suspicious-semicolon)
-    return JSTaggedValue(Barriers::GetValue<JSTaggedType>(GetData(), offset));
-}
-
-#undef MAYBE_INLINE
-
 template <bool needBarrier>
 inline void TaggedArray::Copy(const JSThread* thread, uint32_t dstStart, uint32_t srcStart,
                               const TaggedArray* srcArray, uint32_t count)
