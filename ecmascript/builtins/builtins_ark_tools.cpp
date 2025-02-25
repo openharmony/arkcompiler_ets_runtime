8b7fbe0c9006c6f0b6c6535142e93ac5e350e6dd
zoumujia <zoumujia0920@163.com>
2025-02-25 15:15:36 +0800

    fix workload jit case timeout
    Issue:https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IBOQRR
    
    Signed-off-by: zoumujia <zoumujia0920@163.com>

--- ecmascript/builtins/builtins_ark_tools.cpp
+++ ecmascript/builtins/builtins_ark_tools.cpp
@@ -1452,6 +1452,7 @@ JSTaggedValue BuiltinsArkTools::WaitJitCompileFinish(EcmaRuntimeCallInfo *info)
     }
     while (!jsFunction->GetMachineCode().IsMachineCodeObject()) {
         // just spin check
+        thread->SetInstallMachineCode(true);
         thread->CheckSafepoint();
     }
     return JSTaggedValue::True();
@@ -1467,6 +1468,7 @@ JSTaggedValue BuiltinsArkTools::WaitAllJitCompileFinish(EcmaRuntimeCallInfo *inf
         return JSTaggedValue::False();
     }
     while (Jit::GetInstance()->GetRunningTaskCnt(thread->GetEcmaVM())) {
+        thread->SetInstallMachineCode(true);
         thread->CheckSafepoint();
     }
     thread->SetPGOProfilerEnable(false);
