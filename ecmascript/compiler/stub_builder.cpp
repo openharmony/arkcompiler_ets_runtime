331e6dfe3c1d519a4b7eb47cee313335a1485a0d
l00809476 <linxin41@huawei.com>
2025-02-26 17:18:46 +0800

    update struct equal profile
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IBP48X
    
    Signed-off-by: l00809476 <linxin41@huawei.com>
    Change-Id: Ibb13e4250f3a0555db6e64b84caddc92d281058c

--- ecmascript/compiler/stub_builder.cpp
+++ ecmascript/compiler/stub_builder.cpp
@@ -6520,6 +6520,7 @@ GateRef StubBuilder::FastStrictEqual(GateRef glue, GateRef left, GateRef right, 
     Label stringCompare(env);
     Label updataPGOTypeWithInternString(env);
     Label bigIntEqualCheck(env);
+    Label undefinedCheck(env);
     Label exit(env);
     BRANCH(TaggedIsNumber(left), &leftIsNumber, &leftIsNotNumber);
     Bind(&leftIsNumber);
@@ -6633,7 +6634,7 @@ GateRef StubBuilder::FastStrictEqual(GateRef glue, GateRef left, GateRef right, 
     {
         Label leftIsBigInt(env);
         Label leftIsNotBigInt(env);
-        BRANCH(TaggedIsBigInt(left), &leftIsBigInt, &exit);
+        BRANCH(TaggedIsBigInt(left), &leftIsBigInt, &undefinedCheck);
         Bind(&leftIsBigInt);
         {
             Label rightIsBigInt(env);
@@ -6647,6 +6648,16 @@ GateRef StubBuilder::FastStrictEqual(GateRef glue, GateRef left, GateRef right, 
     Bind(&updataPGOTypeWithInternString);
     {
         callback.ProfileOpType(TaggedInt(PGOSampleType::InternStringType()));
+        Jump(&exit);
+    }
+    Bind(&undefinedCheck);
+    {
+        if (!callback.IsEmpty()) {
+            Label updateProfileOpTypeWithAny(env);
+            BRANCH(TaggedIsUndefined(left), &updateProfileOpTypeWithAny, &exit);
+            Bind(&updateProfileOpTypeWithAny);
+            callback.ProfileOpType(TaggedInt(PGOSampleType::UndefinedOrNullType()));
+        }
         Jump(&exit);
     }
     Bind(&exit);
