68c387029be7c118dc6738dda9a12f51332083c0
xusen <xusen15@h-partners.com>
2025-03-14 20:25:39 +0800

    Revert ason stringify undefined
    Recert ason stringify undefined
    
    Issue:  https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IBTIIR
    Signed-off-by: xusen <xusen15@h-partners.com>

--- ecmascript/base/json_stringifier.cpp
+++ ecmascript/base/json_stringifier.cpp
@@ -649,9 +649,6 @@ bool JsonStringifier::SerializeJSONHashMap(const JSHandle<JSTaggedValue> &value,
         }
         keyHandle.Update(node->GetKey());
         valueHandle.Update(node->GetValue());
-        if (valueHandle->IsUndefined()) {
-            continue;
-        }
         if (UNLIKELY(!keyHandle->IsString())) {
             result_ += "\"";
             SerializeJSONProperty(keyHandle, replacer);
@@ -701,10 +698,7 @@ bool JsonStringifier::SerializeJSONHashSet(const JSHandle<JSTaggedValue> &value,
             continue;
         }
         currentKey.Update(node->GetKey());
-        JSTaggedValue res = SerializeJSONProperty(currentKey, replacer);
-        if (res.IsUndefined()) {
-            result_ += "null";
-        }
+        SerializeJSONProperty(currentKey, replacer);
         result_ += ",";
         needRemove = true;
     }
@@ -738,12 +732,10 @@ bool JsonStringifier::SerializeLinkedHashMap(const JSHandle<LinkedHashMap> &hash
     while (index < totalElements) {
         keyHandle.Update(hashMap->GetKey(index++));
         valHandle.Update(hashMap->GetValue(index - 1));
-        if (keyHandle->IsHole() || valHandle->IsUndefined()) {
+        if (keyHandle->IsHole()) {
             continue;
         }
-        if (UNLIKELY(keyHandle->IsUndefined())) {
-            result_ += "\"undefined\"";
-        } else if (UNLIKELY(!keyHandle->IsString())) {
+        if (UNLIKELY(!keyHandle->IsString())) {
             result_ += "\"";
             SerializeJSONProperty(keyHandle, replacer);
             result_ += "\"";
@@ -787,10 +779,7 @@ bool JsonStringifier::SerializeLinkedHashSet(const JSHandle<LinkedHashSet> &hash
         if (keyHandle->IsHole()) {
             continue;
         }
-        JSTaggedValue res = SerializeJSONProperty(keyHandle, replacer);
-        if (res.IsUndefined()) {
-            result_ += "null";
-        }
+        SerializeJSONProperty(keyHandle, replacer);
         result_ += ",";
         needRemove = true;
     }
