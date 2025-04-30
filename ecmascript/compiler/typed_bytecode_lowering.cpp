4c1bc4da9074557c2648ced2b062240be9980c29
w00443755 <wangzhilei2@huawei.com>
2025-04-30 10:02:15 +0800

    Remove JSFunctionBase::IsCompiledCodeBit Check in jscall-call check optimization
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IC52OJ?from=project-issue
    Change-Id: I54f272e8a5c6da26e26151410d82707b41067e22
    Signed-off-by: w00443755 <wangzhilei2@huawei.com>

--- ecmascript/compiler/typed_bytecode_lowering.cpp
+++ ecmascript/compiler/typed_bytecode_lowering.cpp
@@ -2115,8 +2115,6 @@ void TypedBytecodeLowering::ConvertCallTargetCheckToHeapConstantCheckAndLowerCal
         builder_.Bind(&exit);
 #endif
         builder_.DeoptCheck(builder_.Equal(func, res), frameState, DeoptType::NOTCALLTARGETHEAPOBJECT);
-        GateRef isCompiled = builder_.JudgeAotAndFastCall(func, CircuitBuilder::JudgeMethodType::HAS_AOT);
-        builder_.DeoptCheck(isCompiled, frameState, DeoptType::CALLTARGETNOTCOMPILED);
     }
     auto *jitCompilationEnv = static_cast<const JitCompilationEnv*>(compilationEnv_);
     JSHandle<JSTaggedValue> heapObject = jitCompilationEnv->GetHeapConstantHandle(heapConstantIndex);
@@ -2370,9 +2368,7 @@ void TypedBytecodeLowering::LowerTypedThisCall(const TypeAccessor &tacc)
     if (methodLiteral == nullptr || !methodLiteral->IsTypedCall()) {
         return;
     }
-    auto heapConstantIndex = tacc.TryGetHeapConstantFunctionIndex(methodId);
-    if (!tacc.IsHotnessFunc() &&
-        heapConstantIndex == JitCompilationEnv::INVALID_HEAP_CONSTANT_INDEX) {
+    if (!tacc.IsHotnessFunc()) {
         return;
     }
     uint32_t argc = tacc.GetArgc();
