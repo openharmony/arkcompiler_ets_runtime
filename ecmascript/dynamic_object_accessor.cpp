6e5016ac13572c11a0e295330fb3e5241e50c2f3
hewei <hewei215@huawei.com>
2025-05-09 15:19:49 +0800

    Add accessor, descriptor, type_converter impl
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/IC6L96
    
    Change-Id: I401c76af1229c0be934c35b5ea06770ac16895ee
    Signed-off-by: hewei <hewei215@huawei.com>

---
+++ ecmascript/dynamic_object_accessor.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ecmascript/js_tagged_value.h"
+#include "ecmascript/dynamic_object_accessor.h"
+#include "common_interfaces/objects/base_object_dispatcher.h"
+
+namespace panda::ecmascript {
+
+DynamicObjectAccessor DynamicObjectAccessor::dynObjectAccessor_;
+
+void DynamicObjectAccessor::Initialize()
+{
+    BaseObjectDispatcher::GetDispatcher().RegisterDynamicObjectAccessor(&dynObjectAccessor_);
+}
+
+bool DynamicObjectAccessor::HasProperty([[maybe_unused]] ThreadHolder *thread,
+                                        [[maybe_unused]] BaseObject *obj,
+                                        [[maybe_unused]] char *name)
+{
+    return false;
+}
+
+JSTaggedValue DynamicObjectAccessor::GetProperty([[maybe_unused]] ThreadHolder *thread,
+                                                 [[maybe_unused]] BaseObject *obj,
+                                                 [[maybe_unused]] char *name)
+{
+    return JSTaggedValue::Undefined();
+}
+
+bool DynamicObjectAccessor::SetProperty([[maybe_unused]] ThreadHolder *thread,
+                                        [[maybe_unused]] BaseObject *obj,
+                                        [[maybe_unused]] char *name,
+                                        [[maybe_unused]] JSTaggedValue value)
+{
+    return false;
+}
+
+bool DynamicObjectAccessor::HasElementByIdx([[maybe_unused]] ThreadHolder *thread,
+                                            [[maybe_unused]] BaseObject *obj,
+                                            [[maybe_unused]] uint32_t index)
+{
+    return false;
+}
+
+JSTaggedValue DynamicObjectAccessor::GetElementByIdx([[maybe_unused]] ThreadHolder *thread,
+                                                     [[maybe_unused]] BaseObject *obj,
+                                                     [[maybe_unused]] uint32_t index)
+{
+    return JSTaggedValue::Undefined();
+}
+
+bool DynamicObjectAccessor::SetElementByIdx([[maybe_unused]] ThreadHolder *thread,
+                                            [[maybe_unused]] BaseObject *obj,
+                                            [[maybe_unused]] uint32_t index,
+                                            [[maybe_unused]] JSTaggedValue value)
+{
+    return false;
+}
+}
