b8e5c5a4c169a277c1c09f06f1873396f7ee59c4
starunvs <xingyu.lin@huawei.com>
2025-05-15 16:36:29 +0800

    fix cmc gc missing read barrier, handle scope
    Signed-off-by: starunvs <xingyu.lin@huawei.com>
    Change-Id: I5f603cbd151b114e9a5a3ac4de7fe9cf7ea1fce5

--- ecmascript/js_handle.h
+++ ecmascript/js_handle.h
@@ -209,12 +209,14 @@ private:
 template <>
 inline JSTaggedValue *JSHandle<JSTaggedValue>::operator->() const
 {
+    Barriers::UpdateSlot(reinterpret_cast<JSTaggedValue*>(GetAddress()), 0);
     return reinterpret_cast<JSTaggedValue *>(GetAddress());
 }
 
 template <>
 inline JSTaggedValue *JSHandle<JSTaggedValue>::operator*() const
 {
+    Barriers::UpdateSlot(reinterpret_cast<JSTaggedValue*>(GetAddress()), 0);
     return reinterpret_cast<JSTaggedValue *>(GetAddress());
 }
 
