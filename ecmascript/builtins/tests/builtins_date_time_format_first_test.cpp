a07b79ddb49c375ae6029d4358ae924ab982dafd
zhangdd_ewan <zhangdongdong50@huawei.com>
2025-06-14 13:06:54 +0800

    icu升级74.2改用例
    Signed-off-by: zhangdd_ewan <zhangdongdong50@huawei.com>

--- ecmascript/builtins/tests/builtins_date_time_format_first_test.cpp
+++ ecmascript/builtins/tests/builtins_date_time_format_first_test.cpp
@@ -197,9 +197,7 @@ HWTEST_F_L0(BuiltinsDateTimeFormatTest, FormatRange_002)
 
     JSHandle<EcmaString> handleStr(thread, result);
     JSHandle<EcmaString> resultStr = factory->NewFromUtf8("Fri, 1/1/2021, 24:00:00 – Mon, 3/1/2021, 24:00:00");
-    JSHandle<EcmaString> oldStr = factory->NewFromUtf8("Fri, 1/1/2021, 24:00:00 – Mon, 3/1/2021, 24:00:00");
-    EXPECT_TRUE(EcmaStringAccessor::Compare(instance, handleStr, resultStr) == 0 ||
-                EcmaStringAccessor::Compare(instance, handleStr, oldStr) == 0);
+    EXPECT_EQ(EcmaStringAccessor::Compare(instance, handleStr, resultStr), 0);
 }
 
 HWTEST_F_L0(BuiltinsDateTimeFormatTest, FormatRangeToParts)
