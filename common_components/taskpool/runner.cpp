ff22860326b1ecfe6e245ca89bfcb31c1cc86ad7
jinjiawei <jinjiawei2@huawei.com>
2025-07-18 14:43:40 +0800

    revert adjust rss Priority
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/ICN83C?from=project-issue
    Signed-off-by: jinjiawei <jinjiawei2@huawei.com>
    Change-Id: Id6b19f85302eb7ca49cc62eede67ad5dd743a7c7

--- common_components/taskpool/runner.cpp
+++ common_components/taskpool/runner.cpp
@@ -20,10 +20,6 @@
 #include "qos.h"
 #endif
 
-#ifdef ENABLE_RSS
-#include "res_sched_client.h"
-#endif
-
 namespace common {
 Runner::Runner(uint32_t threadNum, const std::function<void(native_handle_type)> prologueHook,
     const std::function<void(native_handle_type)> epilogueHook)
@@ -109,21 +105,6 @@ void Runner::SetQosPriority([[maybe_unused]] PriorityMode mode)
         default:
             UNREACHABLE();
             break;
-    }
-#endif
-}
-
-void Runner::SetRssPriority([[maybe_unused]] RssPriorityType type)
-{
-#ifdef ENABLE_RSS
-    std::lock_guard<std::mutex> guard(mtx_);
-    int64_t status = static_cast<int64_t>(type);
-    for (uint32_t threadId : gcThreadId_) {
-        std::unordered_map<std::string, std::string> payLoad = { { "pid", std::to_string(getpid()) },
-                                                    { "tid", std::to_string(threadId) } };
-        OHOS::ResourceSchedule::ResSchedClient::GetInstance()
-            .ReportData(OHOS::ResourceSchedule::ResType::RES_TYPE_GC_THREAD_QOS_STATUS_CHANGE,
-            status, payLoad);
     }
 #endif
 }
