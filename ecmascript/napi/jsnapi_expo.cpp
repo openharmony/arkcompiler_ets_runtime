b5aeaff45a44327db78a155bf2cbc32b06466aa7
yanzhiqi1 <yanzhiqi1@huawei.com>
2025-08-06 21:45:29 +0800

    change level to warn
    Issue: #ICRNZM
    
    Signed-off-by: yanzhiqi1 <yanzhiqi1@huawei.com>
    Change-Id: I5a92c1ddf4026640b1b0be102db9591a371234a6

--- ecmascript/napi/jsnapi_expo.cpp
+++ ecmascript/napi/jsnapi_expo.cpp
@@ -5020,7 +5020,7 @@ void JSNApi::PrintExceptionInfo(const EcmaVM *vm)
     }
     JSHandle<EcmaString> result = JSTaggedValue::ToString(thread, exceptionHandle);
     ecmascript::CString string = ConvertToString(thread, *result);
-    LOG_ECMA(ERROR) << string;
+    LOG_ECMA(WARN) << string;
     ThrowException(vm, exception);
 }
 
@@ -5390,7 +5390,7 @@ bool JSNApi::StopDebugger([[maybe_unused]] EcmaVM *vm)
 
     auto sym = panda::os::library_loader::ResolveSymbol(handle, "StopDebug");
     if (!sym) {
-        LOG_ECMA(ERROR) << sym.Error().ToString();
+        LOG_ECMA(WARN) << sym.Error().ToString();
         return false;
     }
 
