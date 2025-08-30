1aa77cc5f5b03ceed64cd700af83f3a996a5586e
zhouguangyuan0718 <zhouguangyuan1@huawei.com>
2025-08-30 02:08:12 +0000

    Revert "add concurrent verify for stub compiler"
    Issue: https://gitee.com/openharmony/arkcompiler_ets_runtime/issues/ICVJ6S
    Signed-off-by: ZhouGuangyuan <zhouguangyuan1@huawei.com>

--- ecmascript/tests/machine_code_test.cpp
+++ ecmascript/tests/machine_code_test.cpp
@@ -22,9 +22,7 @@
 #include "ecmascript/js_array.h"
 #include "ecmascript/ecma_vm.h"
 #include "ecmascript/object_factory-inl.h"
-#include "ecmascript/compiler/aot_file/aot_file_info.h"
 #include "ecmascript/tests/test_helper.h"
-#include <thread>
 
 using namespace panda;
 using namespace panda::ecmascript;
@@ -228,27 +226,4 @@ HWTEST_F_L0(MachineCodeTest, SetText003)
     Jit::GetInstance()->SetEnableAsyncCopyToFort(true);
 }
 
-HWTEST_F_L0(MachineCodeTest, ConcurrentTest001)
-{
-    std::vector<std::thread> threads;
-    for (size_t i = 1; i < 10; ++i) {
-        threads.emplace_back([] {
-            ConcurrentMonitor::Scope concurrentScope;
-            sleep(1);
-            ConcurrentMonitor::monitor_.ArriveAndWait();
-            sleep(1);
-        });
-    }
-    {
-        ConcurrentMonitor::Scope concurrentScope;
-        sleep(1);
-        ConcurrentMonitor::monitor_.ArriveAndWait();
-        sleep(1);
-    }
-    for (auto &t: threads) {
-        if (t.joinable()) {
-            t.join();
-        }
-    }
-}
 }
