6c43273197659e468fa29d5b120f2edce6f6db4b
yaochaonan <yaochaonan@huawei.com>
2025-11-06 16:47:20 +0800

    Fix moduleSnapshot change status
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_runtime/issues/11860
    
    Signed-off-by: yaochaonan <yaochaonan@huawei.com>
    Change-Id: I9a542e933ccf57c38533112ecc14a3fc8eff9de0

--- ecmascript/napi/jsnapi_expo.cpp
+++ ecmascript/napi/jsnapi_expo.cpp
@@ -6274,6 +6274,7 @@ void JSNApi::SetReleaseSecureMemCallback(ReleaseSecureMemCallback releaseSecureM
 // Arkui trigger jsPandafile Seralize when cold start is end.
 void JSNApi::PandaFileSerialize(const EcmaVM *vm)
 {
+    ecmascript::ThreadManagedScope scope(vm->GetJSThread());
     if (const_cast<EcmaVM *>(vm)->GetJSOptions().DisableJSPandaFileAndModuleSnapshot()) {
         return;
     }
@@ -6284,6 +6285,7 @@ void JSNApi::PandaFileSerialize(const EcmaVM *vm)
 // Arkui trigger module Seralize when cold start is end.
 void JSNApi::ModuleSerialize(const EcmaVM *vm)
 {
+    ecmascript::ThreadManagedScope scope(vm->GetJSThread());
     JSRuntimeOptions &options = const_cast<EcmaVM *>(vm)->GetJSOptions();
     if (options.DisableJSPandaFileAndModuleSnapshot() || options.DisableModuleSnapshot()) {
         return;
@@ -6295,6 +6297,7 @@ void JSNApi::ModuleSerialize(const EcmaVM *vm)
 // Ability Runtime trigger module Deseralize when application start.
 void JSNApi::ModuleDeserialize(EcmaVM *vm, const uint32_t appVersion)
 {
+    ecmascript::ThreadManagedScope scope(vm->GetJSThread());
     if (vm->GetJSOptions().DisableJSPandaFileAndModuleSnapshot()) {
         return;
     }
