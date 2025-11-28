9a8668d7e74ecc186fad9274c491057d6f9420e0
zhao1d <zhaolidong4@huawei.com>
2025-11-28 15:48:31 +0800

    Supplementary base_string_table.cpp's testcase
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_runtime/issues/11939
    Signed-off-by: zhao1d <zhaolidong4@huawei.com>
    Change-Id: Ia598fb24e8dcf5a01b27f60b1fc8a1ee1a28040d

--- ecmascript/tests/base_string_table_test.cpp
+++ ecmascript/tests/base_string_table_test.cpp
@@ -131,4 +131,403 @@ HWTEST_F_L0(BaseStringTableTest, GetOrInternStringFromCompressedSubString_SubStr
                  "0x680x650x6c0x6c0x6f0x200x770x6f0x720x6c0x64");
     EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(internStr)).IsInternString());
 }
+
+/**
+ * @tc.name: GetOrInternString_EmptyStringWithCompression
+ * @tc.desc: Test interning empty string with compression enabled
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_EmptyStringWithCompression)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    uint8_t emptyData[] = {0};
+    ecmascript::BaseString* result = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, emptyData, 0, true);
+    EXPECT_NE(result, nullptr);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(result)).ToCString(thread).c_str(), "");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternString_EmptyStringWithoutCompression
+ * @tc.desc: Test interning empty string with compression disabled
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_EmptyStringWithoutCompression)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    uint8_t emptyData[] = {0};
+    ecmascript::BaseString* result = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, emptyData, 0, false);
+    EXPECT_NE(result, nullptr);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(result)).ToCString(thread).c_str(), "");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternString_SingleCharacter
+ * @tc.desc: Test interning single character strings
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_SingleCharacter)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    uint8_t singleChar[] = {'A'};
+    ecmascript::BaseString* result = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, singleChar, sizeof(singleChar), true);
+    EXPECT_NE(result, nullptr);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(result)).ToCString(thread).c_str(), "A");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternString_LongString
+ * @tc.desc: Test interning long string (multiple segments)
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_LongString)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    std::string longString(100, 'x'); // 100 character string
+    ecmascript::BaseString* result = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_,
+        reinterpret_cast<const uint8_t*>(longString.c_str()),
+        longString.length(), true);
+    EXPECT_NE(result, nullptr);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(result)).ToCString(thread).c_str(),
+                 longString.c_str());
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternString_RepeatedString
+ * @tc.desc: Test interning same string multiple times returns same object
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_RepeatedString)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    uint8_t testData[] = {'d', 'u', 'p', 'l', 'i', 'c', 'a', 't', 'e'};
+
+    ecmascript::BaseString* result1 = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, testData, sizeof(testData), true);
+    ecmascript::BaseString* result2 = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, testData, sizeof(testData), true);
+
+    EXPECT_EQ(result1, result2);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(result1)).ToCString(thread).c_str(), "duplicate");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result1)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternString_NumericString
+ * @tc.desc: Test interning numeric string
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_NumericString)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    uint8_t numericData[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+    ecmascript::BaseString* result = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, numericData, sizeof(numericData), true);
+    EXPECT_NE(result, nullptr);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(result)).ToCString(thread).c_str(), "1234567890");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternString_SpecialCharacters
+ * @tc.desc: Test interning string with special ASCII characters
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_SpecialCharacters)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    uint8_t specialData[] = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')'};
+    ecmascript::BaseString* result = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, specialData, sizeof(specialData), true);
+    EXPECT_NE(result, nullptr);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(result)).ToCString(thread).c_str(), "!@#$%^&*()");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternString_WhitespaceString
+ * @tc.desc: Test interning string with only whitespace characters
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_WhitespaceString)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    uint8_t whitespaceData[] = {' ', '\t', '\n', '\r', ' ', '\f'};
+    ecmascript::BaseString* result = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, whitespaceData, sizeof(whitespaceData), true);
+    EXPECT_NE(result, nullptr);
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternStringFromCompressedSubString_FullString
+ * @tc.desc: Test creating substring from entire string (offset 0)
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternStringFromCompressedSubString_FullString)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    ecmascript::ObjectFactory* factory = thread->GetEcmaVM()->GetFactory();
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    JSHandle<EcmaString> originalStr = factory->NewFromASCII("testcase");
+    uint32_t offset = 0;
+    uint32_t utf8Len = EcmaStringAccessor(*originalStr).GetLength();
+
+    ecmascript::BaseString* internStr = table.GetOrInternStringFromCompressedSubString(
+        thread->GetThreadHolder(), handleCreator_, originalStr, offset, utf8Len);
+    EXPECT_NE(internStr, nullptr);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(internStr)).ToCString(thread).c_str(), "testcase");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(internStr)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternStringFromCompressedSubString_OneCharSubstring
+ * @tc.desc: Test creating single character substring
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternStringFromCompressedSubString_OneCharSubstring)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    ecmascript::ObjectFactory* factory = thread->GetEcmaVM()->GetFactory();
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    JSHandle<EcmaString> originalStr = factory->NewFromASCII("abcdef");
+    uint32_t offset = 2;
+    uint32_t utf8Len = 1;
+
+    ecmascript::BaseString* internStr = table.GetOrInternStringFromCompressedSubString(
+        thread->GetThreadHolder(), handleCreator_, originalStr, offset, utf8Len);
+    EXPECT_NE(internStr, nullptr);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(internStr)).ToCString(thread).c_str(), "c");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(internStr)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternStringFromCompressedSubString_RepeatedSubstring
+ * @tc.desc: Test creating same substring multiple times
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternStringFromCompressedSubString_RepeatedSubstring)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    ecmascript::ObjectFactory* factory = thread->GetEcmaVM()->GetFactory();
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    JSHandle<EcmaString> originalStr1 = factory->NewFromASCII("firsttest");
+    JSHandle<EcmaString> originalStr2 = factory->NewFromASCII("secondtest");
+    uint32_t offset = 5;
+    uint32_t utf8Len = 4;
+
+    ecmascript::BaseString* internStr1 = table.GetOrInternStringFromCompressedSubString(
+        thread->GetThreadHolder(), handleCreator_, originalStr1, offset, utf8Len);
+    ecmascript::BaseString* internStr2 = table.GetOrInternStringFromCompressedSubString(
+        thread->GetThreadHolder(), handleCreator_, originalStr2, offset, utf8Len);
+
+    EXPECT_EQ(internStr1, internStr2);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(internStr1)).ToCString(thread).c_str(), "test");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(internStr1)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternString_HashCollisionTest
+ * @tc.desc: Test strings that might cause hash collisions
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_HashCollisionTest)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    // Strings that might have similar hash values
+    uint8_t data1[] = {'A', 'a', '1'};
+    uint8_t data2[] = {'B', 'B', '1'};
+
+    ecmascript::BaseString* result1 = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, data1, sizeof(data1), true);
+    ecmascript::BaseString* result2 = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, data2, sizeof(data2), true);
+
+    EXPECT_NE(result1, result2);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(result1)).ToCString(thread).c_str(), "Aa1");
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(result2)).ToCString(thread).c_str(), "BB1");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result1)).IsInternString());
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result2)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternString_UpperCaseLowerCase
+ * @tc.desc: Test interning strings with different cases
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_UpperCaseLowerCase)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    uint8_t upperCase[] = {'H', 'E', 'L', 'L', 'O'};
+    uint8_t lowerCase[] = {'h', 'e', 'l', 'l', 'o'};
+
+    ecmascript::BaseString* resultUpper = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, upperCase, sizeof(upperCase), true);
+    ecmascript::BaseString* resultLower = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, lowerCase, sizeof(lowerCase), true);
+
+    EXPECT_NE(resultUpper, resultLower);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(resultUpper)).ToCString(thread).c_str(), "HELLO");
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(resultLower)).ToCString(thread).c_str(), "hello");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(resultUpper)).IsInternString());
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(resultLower)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternString_LargeDataSet
+ * @tc.desc: Test interning many different strings
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_LargeDataSet)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    std::vector<ecmascript::BaseString*> results;
+    const int numStrings = 50;
+
+    for (int i = 0; i < numStrings; i++) {
+        std::string str = "string" + std::to_string(i);
+        ecmascript::BaseString* result = table.GetOrInternString(
+            thread->GetThreadHolder(), handleCreator_,
+            reinterpret_cast<const uint8_t*>(str.c_str()),
+            str.length(), true);
+        EXPECT_NE(result, nullptr);
+        EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(result)).IsInternString());
+        results.push_back(result);
+    }
+
+    // Verify all strings are unique
+    for (int i = 0; i < numStrings; i++) {
+        std::string str = "string" + std::to_string(i);
+        EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(results[i])).ToCString(thread).c_str(),
+                     str.c_str());
+    }
+}
+
+/**
+ * @tc.name: GetOrInternString_MixedCompression
+ * @tc.desc: Test interning same data with different compression settings
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternString_MixedCompression)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    uint8_t testData[] = {'c', 'o', 'm', 'p', 'r', 'e', 's', 's'};
+
+    ecmascript::BaseString* resultCompressed = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, testData, sizeof(testData), true);
+    ecmascript::BaseString* resultUncompressed = table.GetOrInternString(
+        thread->GetThreadHolder(), handleCreator_, testData, sizeof(testData), false);
+
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(
+        resultCompressed)).ToCString(thread).c_str(), "compress");
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(
+        resultUncompressed)).ToCString(thread).c_str(), "compress");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(resultCompressed)).IsInternString());
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(resultUncompressed)).IsInternString());
+}
+
+/**
+ * @tc.name: GetOrInternStringFromCompressedSubString_EmptySubstring
+ * @tc.desc: Test creating empty substring from existing string
+ * @tc.type: FUNC
+ * @tc.require: AR001
+ */
+HWTEST_F_L0(BaseStringTableTest, GetOrInternStringFromCompressedSubString_EmptySubstring)
+{
+    if (!ecmascript::g_isEnableCMCGC) {
+        return;
+    }
+    ecmascript::ObjectFactory* factory = thread->GetEcmaVM()->GetFactory();
+    auto& table = ecmascript::Runtime::GetInstance()->GetBaseStringTable();
+
+    JSHandle<EcmaString> originalStr = factory->NewFromASCII("content");
+    uint32_t offset = 3;
+    uint32_t utf8Len = 0;
+
+    ecmascript::BaseString* internStr = table.GetOrInternStringFromCompressedSubString(
+        thread->GetThreadHolder(), handleCreator_, originalStr, offset, utf8Len);
+    EXPECT_NE(internStr, nullptr);
+    EXPECT_STREQ(EcmaStringAccessor(EcmaString::FromBaseString(internStr)).ToCString(thread).c_str(), "");
+    EXPECT_TRUE(EcmaStringAccessor(EcmaString::FromBaseString(internStr)).IsInternString());
+}
+
 } // namespace panda::test
