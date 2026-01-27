db61fded2000875e5a8e62ae91fac27b98a2fdd4
杨云飞 <yangyunfei19@h-partners.com>
2026-01-27 14:20:40 +0800

    整改moduletest用例
    Issue:https://gitcode.com/openharmony/arkcompiler_ets_runtime/issues/11812
    
    Signed-off-by: 杨云飞 <yangyunfei19@h-partners.com>

--- test/moduletest/getpropertybyindex/getpropertybyindex.js
+++ test/moduletest/getpropertybyindex/getpropertybyindex.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,33 +19,37 @@
  * @tc.type: FUNC
  * @tc.require: issueI5NO8G
  */
-const v2 = new Int16Array(11);
-const t3 = v2.__proto__;
-let flag1 = false;
-try {
-  delete t3?.[9];
-} catch (e) {
-  flag1 = true;
-}
-print(flag1);
-
-let flag2 = true;
-let result = [];
-var arr = new Array(20)
-for (let i = 0; i < 10; i++) {
-  arr[i] = i;
-
-}
-let obj = new Int8Array(arr);
-for (var i = 0; i < 10; i++) {
-  result.push(obj[i] == i);
-}
-for (var i = 10; i < 20; i++) {
-  result.push(obj[i] == 0);
-}
-for (let i = 0; i < result.length; i++) {
-  if (!result[i]) {
-    flag2 = false;
+{
+  const v2 = new Int16Array(11);
+  const t3 = v2.__proto__;
+  let flag1 = false;
+  try {
+    delete t3?.[9];
+  } catch (e) {
+    flag1 = true;
   }
+  assert_equal(flag1, false);
+
+  let flag2 = true;
+  let result = [];
+  var arr = new Array(20)
+  for (let i = 0; i < 10; i++) {
+    arr[i] = i;
+
+  }
+  let obj = new Int8Array(arr);
+  for (var i = 0; i < 10; i++) {
+    result.push(obj[i] == i);
+  }
+  for (var i = 10; i < 20; i++) {
+    result.push(obj[i] == 0);
+  }
+  for (let i = 0; i < result.length; i++) {
+    if (!result[i]) {
+      flag2 = false;
+    }
+  }
+  assert_equal(flag2, true);
 }
-print(flag2);
+
+test_end();
