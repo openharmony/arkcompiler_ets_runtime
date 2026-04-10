d12289f0ebc618747674c14c53976bdf43b082dd
xuziming <xuziming4@h-partners.com>
2026-04-10 16:36:31 +0800

    Set.prototype.add处理
    Issue:https://gitcode.com/openharmony/arkcompiler_ets_runtime/issues/12835
    
    Co-Authored-By: Agent
    
    Signed-off-by: xuziming <xuziming4@h-partners.com>

--- ecmascript/js_set.cpp
+++ ecmascript/js_set.cpp
@@ -15,24 +15,47 @@
 
 #include "ecmascript/js_set.h"
 #include "ecmascript/linked_hash_table.h"
+#include <cmath>
 
 namespace panda::ecmascript {
+// CanonicalizeKeyedCollectionKey operation
+// This operation canonicalizes -0 to +0 for keyed collection keys
+// NaN is returned as-is (SameValueZero treats NaN as equal to NaN)
+static inline JSTaggedValue CanonicalizeKeyedCollectionKey(JSTaggedValue key)
+{
+    if (!key.IsDouble()) {
+        return key;
+    }
+    double d = key.GetDouble();
+    // 3.a. If key is -0, return +0
+    // Only canonicalize -0, not all negative numbers like -Infinity
+    if (d == 0.0 && std::signbit(d) != 0) {
+        return JSTaggedValue(0.0);  // NOLINT(readability-magic-numbers)
+    }
+    // 3.b. If key is NaN, return key as-is (SameValueZero handles NaN equality)
+    return key;
+}
+
 void JSSet::Add(JSThread *thread, const JSHandle<JSSet> &set, const JSHandle<JSTaggedValue> &value)
 {
     if (!LinkedHashSet::IsKey(value.GetTaggedValue())) {
         //  throw error
         THROW_TYPE_ERROR(thread, "the value must be Key of JSSet");
     }
+    // 3. Set value to CanonicalizeKeyedCollectionKey(value).
+    JSTaggedValue canonicalValue = CanonicalizeKeyedCollectionKey(value.GetTaggedValue());
     JSHandle<LinkedHashSet> setHandle(thread, LinkedHashSet::Cast(set->GetLinkedSet(thread).GetTaggedObject()));
-
-    JSHandle<LinkedHashSet> newSet = LinkedHashSet::Add(thread, setHandle, value);
+    JSHandle<LinkedHashSet> newSet = LinkedHashSet::Add(
+        thread, setHandle, JSHandle<JSTaggedValue>(thread, canonicalValue));
     set->SetLinkedSet(thread, newSet);
 }
 
 bool JSSet::Delete(const JSThread *thread, const JSHandle<JSSet> &set, const JSHandle<JSTaggedValue> &value)
 {
+    // CanonicalizeKeyedCollectionKey(value)
+    JSTaggedValue canonicalValue = CanonicalizeKeyedCollectionKey(value.GetTaggedValue());
     JSHandle<LinkedHashSet> setHandle(thread, LinkedHashSet::Cast(set->GetLinkedSet(thread).GetTaggedObject()));
-    int entry = setHandle->FindElement(thread, value.GetTaggedValue());
+    int entry = setHandle->FindElement(thread, canonicalValue);
     if (entry == -1) {
         return false;
     }
@@ -50,7 +73,9 @@ void JSSet::Clear(const JSThread *thread, const JSHandle<JSSet> &set)
 
 bool JSSet::Has(const JSThread *thread, JSTaggedValue value) const
 {
-    return LinkedHashSet::Cast(GetLinkedSet(thread).GetTaggedObject())->Has(thread, value);
+    // CanonicalizeKeyedCollectionKey(value)
+    JSTaggedValue canonicalValue = CanonicalizeKeyedCollectionKey(value);
+    return LinkedHashSet::Cast(GetLinkedSet(thread).GetTaggedObject())->Has(thread, canonicalValue);
 }
 
 uint32_t JSSet::GetSize(const JSThread *thread) const
