5f4cf64312ed398936078f9358e025a293c86b84
ningqicheng <ningqicheng@huawei.com>
2026-05-09 11:52:35 +0800

    Fix idle AOT running when the screen is turned on
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_runtime/issues/12947
    
    Co-Authored-By: Agent
    
    Signed-off-by: ningqicheng <ningqicheng@huawei.com>
    Change-Id: Iebc6dc27e2b85704477442813474c0ec7462447b

--- compiler_service/include/aot_compiler_impl.h
+++ compiler_service/include/aot_compiler_impl.h
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+/**
+ * Copyright (c) 2024-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -69,6 +69,7 @@ protected:
     AotCompilerImpl& operator=(AotCompilerImpl&&) = delete;
 protected:
     std::atomic<bool> allowAotCompiler_ {true};
+    std::atomic<bool> stopRequested_ {false};
     mutable std::mutex stateMutex_;
     std::unique_ptr<AOTArgsHandler> argsHandler_;
     struct AOTState {
