a5b1c3e0e7ba8181ca161e021f4d4c3455b2997e
huanghello <huangzhenghua3@huawei.com>
2026-06-23 11:42:23 +0800

    fix js_meta_data ut fail in 32-bit platform
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_runtime/issues/13315
    Co-Authored-By:Agent
    
    Signed-off-by: huanghello <huangzhenghua3@huawei.com>
    Change-Id: Ie395ebaf5e11701e07befc46468c4e14787f68ae

--- ecmascript/dfx/hprof/tests/js_metadata_test.cpp
+++ ecmascript/dfx/hprof/tests/js_metadata_test.cpp
@@ -4420,6 +4420,7 @@ HWTEST_F_L0(JSMetadataTest, TestResolvedindexbindingRecordMetadata)
 
 HWTEST_F_L0(JSMetadataTest, TestResolvedrecordbindingRecordMetadata)
 {
+#if defined(PANDA_TARGET_AMD64) || defined(PANDA_TARGET_ARM64)
     JSMetadataTestHelper tester {};
     std::string metadataFilePath = METADATA_SOURCE_FILE_DIR"resolvedrecordbinding_record.json";
     JSMetadataTestHelper::Metadata metadata {};
@@ -4427,10 +4428,12 @@ HWTEST_F_L0(JSMetadataTest, TestResolvedrecordbindingRecordMetadata)
     tester.ReadAndParseMetadataJson(metadataFilePath, metadata);
     ASSERT_TRUE(metadata.status == JSMetadataTestHelper::INITIALIZED);
     ASSERT_TRUE(tester.Test(JSType::RESOLVEDRECORDBINDING_RECORD, metadata));
+#endif
 }
 
 HWTEST_F_L0(JSMetadataTest, TestResolvedrecordindexbindingRecordMetadata)
 {
+#if defined(PANDA_TARGET_AMD64) || defined(PANDA_TARGET_ARM64)
     JSMetadataTestHelper tester {};
     std::string metadataFilePath = METADATA_SOURCE_FILE_DIR"resolvedrecordindexbinding_record.json";
     JSMetadataTestHelper::Metadata metadata {};
@@ -4438,6 +4441,7 @@ HWTEST_F_L0(JSMetadataTest, TestResolvedrecordindexbindingRecordMetadata)
     tester.ReadAndParseMetadataJson(metadataFilePath, metadata);
     ASSERT_TRUE(metadata.status == JSMetadataTestHelper::INITIALIZED);
     ASSERT_TRUE(tester.Test(JSType::RESOLVEDRECORDINDEXBINDING_RECORD, metadata));
+#endif
 }
 
 HWTEST_F_L0(JSMetadataTest, TestResolvingFunctionsRecordMetadata)
